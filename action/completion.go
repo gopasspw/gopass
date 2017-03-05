c742e825ed0cf00be59ebd5fb791f8123a19006d
deadc0de6 <info@deadc0de.ch>
2017-03-05 18:39:32 +0100

    adding rofi completion

--- action/completion.go
+++ action/completion.go
@@ -98,3 +98,47 @@ func dmenu(list []string) (string, error) {
 
 	return strings.TrimSpace(string(out)), nil
 }
+
+// CompletionRofi returns a script that starts rofi
+// Usage: eval "$(gopass completion rofi)"
+func (s *Action) CompletionRofi(c *cli.Context) error {
+	typeit := c.Bool("type")
+
+	list, err := s.Store.List()
+	if err != nil {
+		return err
+	}
+
+	name, err := rofi(list)
+	if err != nil {
+		return err
+	}
+
+	content, err := s.Store.First(name)
+	if err != nil {
+		return err
+	}
+
+	if typeit {
+		return exec.Command("xdotool", "type", "--clearmodifiers", string(content)).Run()
+	}
+
+	return s.copyToClipboard(name, content)
+}
+
+// rofi runs it with the provided strings and returns the selected string
+func rofi(list []string) (string, error) {
+	stdin := bytes.NewBuffer(nil)
+	for _, v := range list {
+		stdin.WriteString(v + "\n")
+	}
+
+	cmd := exec.Command("rofi", "-dmenu")
+	cmd.Stdin = stdin
+	out, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+
+	return strings.TrimSpace(string(out)), nil
+}
