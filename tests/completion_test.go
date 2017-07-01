d78b249cf48dee598213543ca2b5c37124ec32f6
Ivan Sorokin <ivan@sorokin.io>
2017-07-01 16:31:38 +0200

    Completion respects escaping (#178)

--- tests/completion_test.go
+++ tests/completion_test.go
@@ -22,6 +22,7 @@ PROG=gopass
 _cli_bash_autocomplete() {
      local cur opts base
      COMPREPLY=()
+     local IFS=$'\n'
      cur="${COMP_WORDS[COMP_CWORD]}"
      opts=$( ${COMP_WORDS[@]:0:$COMP_CWORD} --generate-bash-completion )
      COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
