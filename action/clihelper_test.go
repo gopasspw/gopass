f37eb4d291484ad3451da32574ab23075d52388d
marthjod@gmail.com <marthjod@gmail.com>
2017-07-11 21:56:56 +0200

    askForGitConfigUser: add unit test

---
+++ action/clihelper_test.go
@@ -0,0 +1,42 @@
+package action
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_askForGitConfigUser(t *testing.T) {
+	s := New("test-init")
+	s.isTerm = true
+
+	_, _, err := s.askForGitConfigUser()
+	if err == nil {
+		t.Error("Did not return error")
+	}
+}
+
+func Test_askForGitConfigUserNonInteractive(t *testing.T) {
+	s := New("test-init")
+	// for explicitness
+	s.isTerm = false
+
+	keyList, err := s.gpg.ListPrivateKeys()
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	name, email, err := s.askForGitConfigUser()
+	assert.NoError(t, err)
+
+	// tests cannot know whether keyList returned empty or not.
+	// a better distinction would require mocking/patching
+	// calls to s.gpg.ListPrivateKeys()
+	if len(keyList) > 0 {
+		assert.NotEqual(t, "", name)
+		assert.NotEqual(t, "", email)
+	} else {
+		assert.Equal(t, "", name)
+		assert.Equal(t, "", email)
+	}
+}
