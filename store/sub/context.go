efb9be78510ff1b390c06d8c12024e6e8786309e
Dominik Schulz <dominik.schulz@gauner.org>
2017-09-28 10:21:09 +0200

    Fix config value propagation
    Fixes #354

--- store/sub/context.go
+++ store/sub/context.go
@@ -24,6 +24,13 @@ func WithFsckCheck(ctx context.Context, check bool) context.Context {
 	return context.WithValue(ctx, ctxKeyFsckCheck, check)
 }
 
+// HasFsckCheck returns true if a value for fsck check has been set in this
+// context
+func HasFsckCheck(ctx context.Context) bool {
+	_, ok := ctx.Value(ctxKeyFsckCheck).(bool)
+	return ok
+}
+
 // IsFsckCheck returns the value of fsck check
 func IsFsckCheck(ctx context.Context) bool {
 	bv, ok := ctx.Value(ctxKeyFsckCheck).(bool)
@@ -36,6 +43,13 @@ func IsFsckCheck(ctx context.Context) bool {
 // WithFsckForce returns a context with the flag for fsck force set
 func WithFsckForce(ctx context.Context, force bool) context.Context {
 	return context.WithValue(ctx, ctxKeyFsckForce, force)
+}
+
+// HasFsckForce returns true if a value for fsck force has been set in this
+// context
+func HasFsckForce(ctx context.Context) bool {
+	_, ok := ctx.Value(ctxKeyFsckForce).(bool)
+	return ok
 }
 
 // IsFsckForce returns the value of fsck force
@@ -52,6 +66,13 @@ func WithAutoSync(ctx context.Context, sync bool) context.Context {
 	return context.WithValue(ctx, ctxKeyAutoSync, sync)
 }
 
+// HasAutoSync has been set if a value for auto sync has been set in this
+// context
+func HasAutoSync(ctx context.Context) bool {
+	_, ok := ctx.Value(ctxKeyAutoSync).(bool)
+	return ok
+}
+
 // IsAutoSync returns the value of autosync
 func IsAutoSync(ctx context.Context) bool {
 	bv, ok := ctx.Value(ctxKeyAutoSync).(bool)
@@ -66,6 +87,12 @@ func WithReason(ctx context.Context, msg string) context.Context {
 	return context.WithValue(ctx, ctxKeyReason, msg)
 }
 
+// HasReason returns true if a value for reason has been set in this context
+func HasReason(ctx context.Context) bool {
+	_, ok := ctx.Value(ctxKeyReason).(bool)
+	return ok
+}
+
 // GetReason returns the value of reason
 func GetReason(ctx context.Context) string {
 	sv, ok := ctx.Value(ctxKeyReason).(string)
@@ -78,6 +105,13 @@ func GetReason(ctx context.Context) string {
 // WithImportFunc will return a context with the import callback set
 func WithImportFunc(ctx context.Context, imf store.ImportCallback) context.Context {
 	return context.WithValue(ctx, ctxKeyImportFunc, imf)
+}
+
+// HasImportFunc returns true if a value for import func has been set in this
+// context
+func HasImportFunc(ctx context.Context) bool {
+	imf, ok := ctx.Value(ctxKeyImportFunc).(store.ImportCallback)
+	return ok && imf != nil
 }
 
 // GetImportFunc will return the import callback or a default one returning true
@@ -95,6 +129,13 @@ func GetImportFunc(ctx context.Context) store.ImportCallback {
 // WithRecipientFunc will return a context with the recipient callback set
 func WithRecipientFunc(ctx context.Context, imf store.RecipientCallback) context.Context {
 	return context.WithValue(ctx, ctxKeyRecipientFunc, imf)
+}
+
+// HasRecipientFunc returns true if a recipient func has been set in this
+// context
+func HasRecipientFunc(ctx context.Context) bool {
+	imf, ok := ctx.Value(ctxKeyRecipientFunc).(store.RecipientCallback)
+	return ok && imf != nil
 }
 
 // GetRecipientFunc will return the recipient callback or a default one returning
@@ -115,6 +156,12 @@ func WithFsckFunc(ctx context.Context, imf store.FsckCallback) context.Context {
 	return context.WithValue(ctx, ctxKeyFsckFunc, imf)
 }
 
+// HasFsckFunc returns true if a fsck func has been set in this context
+func HasFsckFunc(ctx context.Context) bool {
+	imf, ok := ctx.Value(ctxKeyFsckFunc).(store.FsckCallback)
+	return ok && imf != nil
+}
+
 // GetFsckFunc will return the fsck confirmation callback or a default one
 // returning true.
 // Note: will never return nil
@@ -132,6 +179,13 @@ func GetFsckFunc(ctx context.Context) store.FsckCallback {
 // set
 func WithCheckRecipients(ctx context.Context, cr bool) context.Context {
 	return context.WithValue(ctx, ctxKeyCheckRecipients, cr)
+}
+
+// HasCheckRecipients returns true if check recipients has been set in this
+// context
+func HasCheckRecipients(ctx context.Context) bool {
+	_, ok := ctx.Value(ctxKeyCheckRecipients).(bool)
+	return ok
 }
 
 // IsCheckRecipients will return the value of the check recipients flag or the
