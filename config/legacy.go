1b7173e9bdf81ff167b41aa347008d6a846084f0
Dominik Schulz <dominik.schulz@justwatch.com>
2017-10-05 15:04:53 +0200

    Support pre-1.3.0 configs
    Fixes #381

--- config/legacy.go
+++ config/legacy.go
@@ -44,3 +44,55 @@ func (c *Pre140) Config() *Config {
 	}
 	return cfg
 }
+
+// Pre130 is the gopass config structure before version 1.3.0. Not all fields were
+// available between 1.0.0 and 1.3.0, but this struct should cover all of them.
+type Pre130 struct {
+	AlwaysTrust bool              `yaml:"alwaystrust"` // always trust public keys when encrypting
+	AskForMore  bool              `yaml:"askformore"`  // ask for more data on generate
+	AutoImport  bool              `yaml:"autoimport"`  // import missing public keys w/o asking
+	AutoPull    bool              `yaml:"autopull"`    // pull from git before push
+	AutoPush    bool              `yaml:"autopush"`    // push to git remote after commit
+	ClipTimeout int               `yaml:"cliptimeout"` // clear clipboard after seconds
+	Debug       bool              `yaml:"debug"`       // enable debug output
+	LoadKeys    bool              `yaml:"loadkeys"`    // load missing keys from store
+	Mounts      map[string]string `yaml:"mounts,omitempty"`
+	NoColor     bool              `yaml:"nocolor"`     // disable colors in output
+	NoConfirm   bool              `yaml:"noconfirm"`   // do not confirm recipients when encrypting
+	Path        string            `yaml:"path"`        // path to the root store
+	PersistKeys bool              `yaml:"persistkeys"` // store recipient keys in store
+	SafeContent bool              `yaml:"safecontent"` // avoid showing passwords in terminal
+	Version     string            `yaml:"version"`
+
+	// Catches all undefined files and must be empty after parsing
+	XXX map[string]interface{} `yaml:",inline"`
+}
+
+// CheckOverflow implements configer
+func (c *Pre130) CheckOverflow() error {
+	return checkOverflow(c.XXX, "config")
+}
+
+// Config converts the Pre130 config to the current config struct
+func (c *Pre130) Config() *Config {
+	sc := StoreConfig{
+		AskForMore:  c.AskForMore,
+		AutoImport:  c.AutoImport,
+		AutoSync:    c.AutoPull && c.AutoPush,
+		ClipTimeout: c.ClipTimeout,
+		NoConfirm:   c.NoConfirm,
+		Path:        c.Path,
+		SafeContent: c.SafeContent,
+	}
+	cfg := &Config{
+		Root:    &sc,
+		Mounts:  make(map[string]*StoreConfig, len(c.Mounts)),
+		Version: c.Version,
+	}
+	for k, v := range c.Mounts {
+		subSc := sc
+		subSc.Path = v
+		cfg.Mounts[k] = &subSc
+	}
+	return cfg
+}
