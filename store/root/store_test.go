e9c3a281d3fbb4fe6d938fc051ade8ee215eb14e
Smaug <SmaugTheGreat@users.noreply.github.com>
2017-10-27 11:35:32 +0200

    Fixed pathes under windows (#431)

--- store/root/store_test.go
+++ store/root/store_test.go
@@ -10,6 +10,8 @@ import (
 	"strings"
 	"testing"
 
+	"path"
+
 	"github.com/justwatchcom/gopass/config"
 	"github.com/justwatchcom/gopass/store/sub"
 )
@@ -59,7 +61,7 @@ func TestListMulti(t *testing.T) {
 	}()
 
 	// root store
-	_, ents, err := createStore(filepath.Join(tempdir, "root"))
+	_, ents, err := createStore(path.Join(tempdir, "root"))
 	if err != nil {
 		t.Fatalf("Failed to init root store: %s", err)
 	}
@@ -70,7 +72,7 @@ func TestListMulti(t *testing.T) {
 		t.Fatalf("Failed to init sub1 store: %s", err)
 	}
 	for _, k := range sub1Ent {
-		ents = append(ents, filepath.Join("sub1",k))
+		ents = append(ents, path.Join("sub1", k))
 	}
 
 	// sub2 store
@@ -79,7 +81,7 @@ func TestListMulti(t *testing.T) {
 		t.Fatalf("Failed to init sub2 store: %s", err)
 	}
 	for _, k := range sub2Ent {
-		ents = append(ents, filepath.Join("sub2", k))
+		ents = append(ents, path.Join("sub2", k))
 	}
 	sort.Strings(ents)
 
@@ -94,10 +96,10 @@ func TestListMulti(t *testing.T) {
 	if err != nil {
 		t.Fatalf("failed to create root store: %s", err)
 	}
-	if err := rs.AddMount(ctx, "sub1", filepath.Join(tempdir,"sub1")); err != nil {
+	if err := rs.AddMount(ctx, "sub1", filepath.Join(tempdir, "sub1")); err != nil {
 		t.Fatalf("failed to add mount %s: %s", "sub1", err)
 	}
-	if err := rs.AddMount(ctx, "sub2", filepath.Join(tempdir,"sub2")); err != nil {
+	if err := rs.AddMount(ctx, "sub2", filepath.Join(tempdir, "sub2")); err != nil {
 		t.Fatalf("failed to add mount %s: %s", "sub2", err)
 	}
 	tree, err := rs.Tree()
@@ -128,7 +130,7 @@ func TestListNested(t *testing.T) {
 		t.Fatalf("Failed to init sub1 store: %s", err)
 	}
 	for _, k := range sub1Ent {
-		ents = append(ents, filepath.Join("sub1",k))
+		ents = append(ents, path.Join("sub1", k))
 	}
 	// sub2 store
 	_, sub2Ent, err := createStore(filepath.Join(tempdir, "sub2"))
@@ -136,7 +138,7 @@ func TestListNested(t *testing.T) {
 		t.Fatalf("Failed to init sub2 store: %s", err)
 	}
 	for _, k := range sub2Ent {
-		ents = append(ents, filepath.Join("sub2", k))
+		ents = append(ents, path.Join("sub2", k))
 	}
 	// sub3 store
 	_, sub3Ent, err := createStore(filepath.Join(tempdir, "sub3"))
@@ -144,7 +146,7 @@ func TestListNested(t *testing.T) {
 		t.Fatalf("Failed to init sub3 store: %s", err)
 	}
 	for _, k := range sub3Ent {
-		ents = append(ents, filepath.Join("sub2", "sub3", k))
+		ents = append(ents, path.Join("sub2", "sub3", k))
 	}
 	sort.Strings(ents)
 
@@ -162,7 +164,7 @@ func TestListNested(t *testing.T) {
 	if err := rs.AddMount(ctx, "sub2", filepath.Join(tempdir, "sub2")); err != nil {
 		t.Fatalf("failed to add mount %s: %s", "sub2", err)
 	}
-	if err := rs.AddMount(ctx, filepath.Join("sub2", "sub3"), filepath.Join(tempdir, "sub3")); err != nil {
+	if err := rs.AddMount(ctx, "sub2/sub3", filepath.Join(tempdir, "sub3")); err != nil {
 		t.Fatalf("failed to add mount %s: %s", "sub2", err)
 	}
 	tree, err := rs.Tree()
@@ -171,6 +173,14 @@ func TestListNested(t *testing.T) {
 	}
 	t.Log(tree.Format(100))
 	compareLists(t, ents, tree.List(0))
+}
+
+func allPathsToSlash(paths []string) []string {
+	r := make([]string, len(paths))
+	for i, p := range paths {
+		r[i] = filepath.ToSlash(p)
+	}
+	return r
 }
 
 func createStore(dir string) ([]string, []string, error) {
@@ -186,14 +196,14 @@ func createStore(dir string) ([]string, []string, error) {
 	for _, file := range list {
 		filename := filepath.Join(dir, file+".gpg")
 		if err := os.MkdirAll(filepath.Dir(filename), 0700); err != nil {
-			return recipients, list, err
+			return recipients, allPathsToSlash(list), err
 		}
 		if err := ioutil.WriteFile(filename, []byte{}, 0644); err != nil {
-			return recipients, list, err
+			return recipients, allPathsToSlash(list), err
 		}
 	}
 	err := ioutil.WriteFile(filepath.Join(dir, sub.GPGID), []byte(strings.Join(recipients, "\n")), 0600)
-	return recipients, list, err
+	return recipients, allPathsToSlash(list), err
 }
 
 func maxLenStr(l []string) string {
