4802d7fe12abeeb25bfda3c194211153793380de
Aaron Bieber <aaron@bolddaemon.com>
2017-11-17 09:02:00 -0700

    Bail early if we protector is enabled (#470)
    As pointed out in #469, the pledging breaks the version check.
    My testing "worked" because of the HEAD check..
    
    Since we typically disable remote version checks in OpenBSD anyway
    (doing an update would break the package otherwise), we might as
    well disable it here.

--- utils/protect/protect.go
+++ utils/protect/protect.go
@@ -2,6 +2,9 @@
 
 package protect
 
+// ProtectEnabled lets us know if we have protection or not
+var ProtectEnabled = false
+
 // Pledge on any other system than OpenBSD doesn't do anything
 func Pledge(s string) {
 	return
