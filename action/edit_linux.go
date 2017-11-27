f67389f819f4b55df10fc228d386b6fe39653716
Dominik Schulz <dominik.schulz@gmail.com>
2017-11-27 10:08:08 +0100

    Default to vi in Linux (#479)
    Fixes #478

--- action/edit_linux.go
+++ action/edit_linux.go
@@ -2,11 +2,19 @@
 
 package action
 
-import "os"
+import (
+	"os"
+	"os/exec"
+)
 
 func getEditor() string {
 	if ed := os.Getenv("EDITOR"); ed != "" {
 		return ed
 	}
-	return "editor"
+	if p, err := exec.LookPath("editor"); err == nil {
+		return p
+	}
+	// if neither EDITOR is set nor "editor" available we'll just assume that vi
+	// is installed. If this fails the user will have to set $EDITOR
+	return "vi"
 }
