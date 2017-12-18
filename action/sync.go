9d96a7e2fb1d3f6b043becee5884ad915fa7c202
Dominik Schulz <dominik.schulz@gmail.com>
2017-12-18 15:11:33 +0100

    Refactor insert and sync (#533)

--- action/sync.go
+++ action/sync.go
@@ -25,88 +25,14 @@ func (s *Action) Sync(ctx context.Context, c *cli.Context) error {
 	mps := s.Store.MountPoints()
 	mps = append([]string{""}, mps...)
 
+	// sync all stores (root and all mounted sub stores)
 	for _, mp := range mps {
 		numMPs++
-
-		name := mp
-		if mp == "" {
-			name = "<root>"
-		}
-		fmt.Print(color.GreenString("[%s] ", name))
-
-		sub, err := s.Store.GetSubStore(mp)
-		if err != nil {
-			out.Red(ctx, "Failed to get sub store '%s': %s", name, err)
-			continue
-		}
-		if sub == nil {
-			out.Red(ctx, "Failed to get sub stores '%s: nil'", name)
-			continue
-		}
-
-		numMP := 0
-		if l, err := sub.List(""); err == nil {
-			numMP = len(l)
-		}
-
-		fmt.Print("\n   " + color.GreenString("git pull and push ... "))
-		if err := sub.GitPush(ctx, "", ""); err != nil {
-			if errors.Cause(err) == store.ErrGitNoRemote {
-				out.Yellow(ctx, "Skipped (no remote)")
-				out.Debug(ctx, "Failed to push '%s' to it's remote: %s", name, err)
-				continue
-			}
-			out.Red(ctx, "Failed to push '%s' to it's remote: %s", name, err)
-			continue
-		}
-		fmt.Print(color.GreenString("OK"))
-
-		if l, err := sub.List(""); err == nil {
-			diff := len(l) - numMP
-			if diff > 0 {
-				fmt.Print(color.GreenString(" (Added %d entries)", diff))
-			} else if diff < 0 {
-				fmt.Print(color.GreenString(" (Removed %d entries)", -1*diff))
-			} else {
-				fmt.Print(color.GreenString(" (no changes)"))
-			}
-		}
-
-		// import keys
-		fmt.Print("\n   " + color.GreenString("importing missing keys ... "))
-		if err := sub.ImportMissingPublicKeys(ctx); err != nil {
-			out.Red(ctx, "Failed to import missing public keys for '%s': %s", name, err)
-			continue
-		}
-		fmt.Print(color.GreenString("OK"))
-
-		// export keys
-		fmt.Print("\n   " + color.GreenString("exporting missing keys ... "))
-		rs, err := sub.GetRecipients(ctx, "")
-		if err != nil {
-			out.Red(ctx, "Failed to load recipients for '%s': %s", name, err)
-			continue
-		}
-		exported, err := sub.ExportMissingPublicKeys(ctx, rs)
-		if err != nil {
-			out.Red(ctx, "Failed to export missing public keys for '%s': %s", name, err)
-			continue
-		}
-
-		// only run second push if we did export any keys
-		if exported {
-			if err := sub.GitPush(ctx, "", ""); err != nil {
-				out.Red(ctx, "Failed to push '%s' to it's remote: %s", name, err)
-				continue
-			}
-			fmt.Print(color.GreenString("OK"))
-		} else {
-			fmt.Print(color.GreenString("nothing to do"))
-		}
-		fmt.Println("\n   " + color.GreenString("done"))
+		_ = s.syncMount(ctx, mp)
 	}
 	out.Green(ctx, "All done")
 
+	// calculate number of changes entries
 	if l, err := s.Store.Tree(); err == nil {
 		numEntries = len(l.List(0)) - numEntries
 	}
@@ -118,5 +44,87 @@ func (s *Action) Sync(ctx context.Context, c *cli.Context) error {
 	}
 	_ = notify.Notify("gopass - sync", fmt.Sprintf("Finished. Synced %d remotes.%s", numMPs, diff))
 
+	return nil
+}
+
+func (s *Action) syncMount(ctx context.Context, mp string) error {
+	name := mp
+	if mp == "" {
+		name = "<root>"
+	}
+	fmt.Print(color.GreenString("[%s] ", name))
+
+	sub, err := s.Store.GetSubStore(mp)
+	if err != nil {
+		out.Red(ctx, "Failed to get sub store '%s': %s", name, err)
+		return fmt.Errorf("failed to get sub stores (%s)", err)
+	}
+
+	if sub == nil {
+		out.Red(ctx, "Failed to get sub stores '%s: nil'", name)
+		return fmt.Errorf("failed to get sub stores (nil)")
+	}
+
+	numMP := 0
+	if l, err := sub.List(""); err == nil {
+		numMP = len(l)
+	}
+
+	fmt.Print("\n   " + color.GreenString("git pull and push ... "))
+	if err := sub.GitPush(ctx, "", ""); err != nil {
+		if errors.Cause(err) == store.ErrGitNoRemote {
+			out.Yellow(ctx, "Skipped (no remote)")
+			out.Debug(ctx, "Failed to push '%s' to it's remote: %s", name, err)
+			return err
+		}
+
+		out.Red(ctx, "Failed to push '%s' to it's remote: %s", name, err)
+		return err
+	}
+	fmt.Print(color.GreenString("OK"))
+
+	if l, err := sub.List(""); err == nil {
+		diff := len(l) - numMP
+		if diff > 0 {
+			fmt.Print(color.GreenString(" (Added %d entries)", diff))
+		} else if diff < 0 {
+			fmt.Print(color.GreenString(" (Removed %d entries)", -1*diff))
+		} else {
+			fmt.Print(color.GreenString(" (no changes)"))
+		}
+	}
+
+	// import keys
+	fmt.Print("\n   " + color.GreenString("importing missing keys ... "))
+	if err := sub.ImportMissingPublicKeys(ctx); err != nil {
+		out.Red(ctx, "Failed to import missing public keys for '%s': %s", name, err)
+		return err
+	}
+	fmt.Print(color.GreenString("OK"))
+
+	// export keys
+	fmt.Print("\n   " + color.GreenString("exporting missing keys ... "))
+	rs, err := sub.GetRecipients(ctx, "")
+	if err != nil {
+		out.Red(ctx, "Failed to load recipients for '%s': %s", name, err)
+		return err
+	}
+	exported, err := sub.ExportMissingPublicKeys(ctx, rs)
+	if err != nil {
+		out.Red(ctx, "Failed to export missing public keys for '%s': %s", name, err)
+		return err
+	}
+
+	// only run second push if we did export any keys
+	if exported {
+		if err := sub.GitPush(ctx, "", ""); err != nil {
+			out.Red(ctx, "Failed to push '%s' to it's remote: %s", name, err)
+			return err
+		}
+		fmt.Print(color.GreenString("OK"))
+	} else {
+		fmt.Print(color.GreenString("nothing to do"))
+	}
+	fmt.Println("\n   " + color.GreenString("done"))
 	return nil
 }
