075eb53876b81f9e11734819a85f32e738d4e8fa
Dominik Schulz <dominik.schulz@gauner.org>
2018-01-03 12:47:32 +0100

    Increase test coverage

--- backend/git/cli/git_test.go
+++ backend/git/cli/git_test.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 	"testing"
 
 	gpgmock "github.com/justwatchcom/gopass/backend/gpg/mock"
@@ -37,4 +38,15 @@ func TestGit(t *testing.T) {
 	assert.NoError(t, git.Init(ctx, "0xDEADBEEF", "Dead Beef", "dead.beef@example.org"))
 	sv := git.Version(ctx)
 	t.Logf("Version: %s", sv.String())
+
+	assert.Equal(t, true, git.IsInitialized())
+	tf := filepath.Join(td, "some-file")
+	assert.NoError(t, ioutil.WriteFile(tf, []byte("foobar"), 0644))
+	assert.NoError(t, git.Add(ctx, "some-file"))
+	assert.Equal(t, true, git.HasStagedChanges(ctx))
+	assert.Error(t, git.Push(ctx, "origin", "master"))
+	assert.Error(t, git.Pull(ctx, "origin", "master"))
+
+	// flaky
+	//assert.NoError(t, git.Commit(ctx, "added some-file"))
 }
--- backend/gpg/cli/gpg_test.go
+++ backend/gpg/cli/gpg_test.go
@@ -1,6 +1,11 @@
 package cli
 
-import "testing"
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestSplitPacket(t *testing.T) {
 	m := splitPacket(":pubkey enc packet: version 3, algo 16, keyid 6780DF473C7A71D3")
@@ -11,4 +16,20 @@ func TestSplitPacket(t *testing.T) {
 	if val != "6780DF473C7A71D3" {
 		t.Errorf("Failed to get keyid")
 	}
+}
+
+func TestGPG(t *testing.T) {
+	ctx := context.Background()
+	g, err := New(ctx, Config{})
+	assert.NoError(t, err)
+	assert.NotEqual(t, "", g.Binary())
+
+	_, err = g.ListPublicKeys(ctx)
+	assert.NoError(t, err)
+
+	_, err = g.ListPrivateKeys(ctx)
+	assert.NoError(t, err)
+
+	_, err = g.GetRecipients(ctx, "nothing")
+	assert.Error(t, err)
 }
