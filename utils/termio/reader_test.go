145d9d31061541c5005901512a75ea7d35766b68
Dominik Schulz <dominik.schulz@gmail.com>
2018-01-07 15:01:54 +0100

    Increase test coverage (#621)

--- utils/termio/reader_test.go
+++ utils/termio/reader_test.go
@@ -4,6 +4,7 @@ import (
 	"io"
 	"strings"
 	"testing"
+	"testing/iotest"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -20,6 +21,26 @@ func TestReadLines(t *testing.T) {
 			assert.Equal(t, s, mustReadLine(stdin))
 		}
 	}
+}
+
+func TestReadLineError(t *testing.T) {
+	stdin := strings.NewReader("fo")
+	lr := NewReader(iotest.TimeoutReader(stdin))
+
+	line, err := lr.ReadLine()
+	assert.Error(t, err)
+	assert.Equal(t, "f", line)
+}
+
+func TestRead(t *testing.T) {
+	stdin := strings.NewReader(`foobarbazzabzabzab`)
+	lr := NewReader(stdin)
+
+	b := make([]byte, 10)
+	n, err := lr.Read(b)
+	assert.NoError(t, err)
+	assert.Equal(t, 10, n)
+	assert.Equal(t, "foobarbazz", string(b))
 }
 
 func mustReadLine(r io.Reader) string {
