7443fdc872ac1c5dedf9c3aeb1de0c17f8e5d95a
Dominik Schulz <dominik.schulz@gauner.org>
2018-02-24 21:47:38 +0100

    Also check Path if GPG4Win install location is not found
    Fixes #647

--- backend/crypto/gpg/cli/gpg_windows.go
+++ backend/crypto/gpg/cli/gpg_windows.go
@@ -3,6 +3,7 @@
 package cli
 
 import (
+	"os/exec"
 	"path/filepath"
 
 	"github.com/justwatchcom/gopass/utils/fsutil"
@@ -14,19 +15,27 @@ func detectBinaryCandidates(bin string) ([]string, error) {
 	// gpg.exe for GPG4Win 3.0.0; would be gpg2.exe for 2.x
 	bins := make([]string, 0, 4)
 
-	// try to detect location
+	// try to detect location of installed GPG4Win
 	k, err := registry.OpenKey(registry.LOCAL_MACHINE, `SOFTWARE\GnuPG`, registry.QUERY_VALUE|registry.WOW64_32KEY)
 	if err != nil {
 		return bins, err
 	}
 
-	v, _, err := k.GetStringValue("Install Directory")
-	if err != nil {
-		return bins, err
+	if v, _, err := k.GetStringValue("Install Directory"); err == nil && v != "" {
+		for _, b := range []string{bin, "gpg2.exe", "gpg.exe"} {
+			gpgPath := filepath.Join(v, "bin", b)
+			if fsutil.IsFile(gpgPath) {
+				bins = append(bins, gpgPath)
+			}
+		}
 	}
 
+	// try to detect location for GPG installed somewhere on the PATH
 	for _, b := range []string{bin, "gpg2.exe", "gpg.exe"} {
-		gpgPath := filepath.Join(v, "bin", b)
+		gpgPath, err := exec.LookPath(b)
+		if err != nil {
+			continue
+		}
 		if fsutil.IsFile(gpgPath) {
 			bins = append(bins, gpgPath)
 		}
