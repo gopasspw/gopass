gopasspw/gopass

commit 7ab5453c7105d08e8d460a4940f57346cc2e477e

Do not pre-populate context values (#701)
Fixes #700

action/show.go
@@ -59,7 +59,7 @@ func (s *Action) show(ctx context.Context, c *cli.Context, name, key string, rec
 		return s.showHandleRevision(ctx, c, name, key, GetRevision(ctx))
 	}
 
-	sec, err := s.Store.Get(ctx, name)
+	sec, ctx, err := s.Store.GetContext(ctx, name)
 	if err != nil {
 		return s.showHandleError(ctx, c, name, recurse, err)
 	}

context.go
@@ -14,35 +14,8 @@ import (
 )
 
 func initContext(ctx context.Context, cfg *config.Config) context.Context {
-	// autosync
-	ctx = sub.WithAutoSync(ctx, cfg.Root.AutoSync)
-
 	// always trust
 	ctx = gpg.WithAlwaysTrust(ctx, true)
-
-	// ask for more
-	ctx = ctxutil.WithAskForMore(ctx, cfg.Root.AskForMore)
-
-	// clipboard timeout
-	ctx = ctxutil.WithClipTimeout(ctx, cfg.Root.ClipTimeout)
-
-	// no confirm
-	ctx = ctxutil.WithNoConfirm(ctx, cfg.Root.NoConfirm)
-
-	// no pager
-	ctx = ctxutil.WithNoPager(ctx, cfg.Root.NoPager)
-
-	// show safe content
-	ctx = ctxutil.WithShowSafeContent(ctx, cfg.Root.SafeContent)
-
-	// always use symbols
-	ctx = ctxutil.WithUseSymbols(ctx, cfg.Root.UseSymbols)
-
-	// never use color
-	ctx = ctxutil.WithNoColor(ctx, cfg.Root.NoColor)
-
-	// enable desktop notifications
-	ctx = ctxutil.WithNotifications(ctx, cfg.Root.Notifications)
 
 	// check recipients conflicts with always trust, make sure it's not enabled
 	// when always trust is

store/root/mount.go
@@ -146,7 +146,7 @@ func (r *Store) getStore(ctx context.Context, name string) (context.Context, *su
 	if sub, found := r.mounts[mp]; found {
 		return r.cfg.Mounts[mp].WithContext(ctx), sub, strings.TrimPrefix(name, sub.Alias())
 	}
-	return ctx, r.store, name
+	return r.cfg.Root.WithContext(ctx), r.store, name
 }
 
 // GetSubStore returns an exact match for a mount point or an error if this

store/root/read.go
@@ -12,3 +12,11 @@ func (r *Store) Get(ctx context.Context, name string) (*secret.Secret, error) {
 	ctx, store, name := r.getStore(ctx, name)
 	return store.Get(ctx, name)
 }
+
+// GetContext returns the plaintext and the context of a single key
+func (r *Store) GetContext(ctx context.Context, name string) (*secret.Secret, context.Context, error) {
+	// forward to substore
+	ctx, store, name := r.getStore(ctx, name)
+	sec, err := store.Get(ctx, name)
+	return sec, ctx, err
+}

commit 081af2f9635ef44b9e490e1c6d657618b2b5a674

Fixes #667 and correct a few details  (#705)
* Fixes #667 and correct a few details

This primarily adds the config option requested in #667.
I've added back the notion or erroring in case of a show action on an
entry with only a password and the safe content option enabled
Also I've fixed the way the String() function of the StoreConfig forgot
to display the Notification value in its output.

* Adding tests for autoclip

* adding a clip flag back

* correcting an old copy-paste mistake

* trying to correct the problem

* Correcting the error caused by #701

action/generate.go
@@ -57,7 +57,8 @@ func (s *Action) Generate(ctx context.Context, c *cli.Context) error {
 	}
 
 	// write generated password to store
-	if err := s.generateSetPassword(ctx, name, key, password); err != nil {
+	ctx, err = s.generateSetPassword(ctx, name, key, password)
+	if err != nil {
 		return err
 	}
 
@@ -100,9 +101,10 @@ func (s *Action) generateCopyOrPrint(ctx context.Context, c *cli.Context, name, 
 		)
 		return nil
 	}
-
-	if err := copyToClipboard(ctx, name, []byte(password)); err != nil {
-		return exitError(ctx, ExitIO, err, "failed to copy to clipboard: %s", err)
+	if ctxutil.IsAutoClip(ctx) || c.Bool("clip") {
+		if err := copyToClipboard(ctx, name, []byte(password)); err != nil {
+			return exitError(ctx, ExitIO, err, "failed to copy to clipboard: %s", err)
+		}
 	}
 	return nil
 }
@@ -171,38 +173,40 @@ func (s *Action) generatePasswordXKCD(ctx context.Context, c *cli.Context, lengt
 	return xkcdgen.RandomLengthDelim(pwlen, xkcdSeparator, c.String("xkcdlang"))
 }
 
-func (s *Action) generateSetPassword(ctx context.Context, name, key, password string) error {
+func (s *Action) generateSetPassword(ctx context.Context, name, key, password string) (context.Context, error) {
 	// set a single key in a yaml doc
 	if key != "" {
-		sec, err := s.Store.Get(ctx, name)
+		sec, ctx, err := s.Store.GetContext(ctx, name)
 		if err != nil {
-			return exitError(ctx, ExitEncrypt, err, "failed to set key '%s' of '%s': %s", key, name, err)
+			return ctx, exitError(ctx, ExitEncrypt, err, "failed to set key '%s' of '%s': %s", key, name, err)
 		}
 		if err := sec.SetValue(key, password); err != nil {
-			return exitError(ctx, ExitEncrypt, err, "failed to set key '%s' of '%s': %s", key, name, err)
+			return ctx, exitError(ctx, ExitEncrypt, err, "failed to set key '%s' of '%s': %s", key, name, err)
 		}
 		if err := s.Store.Set(sub.WithReason(ctx, "Generated password for YAML key"), name, sec); err != nil {
-			return exitError(ctx, ExitEncrypt, err, "failed to set key '%s' of '%s': %s", key, name, err)
+			return ctx, exitError(ctx, ExitEncrypt, err, "failed to set key '%s' of '%s': %s", key, name, err)
 		}
-		return nil
+		return ctx, nil
 	}
 
 	// replace password in existing secret
 	if s.Store.Exists(ctx, name) {
-		sec, err := s.Store.Get(ctx, name)
+		sec, ctx, err := s.Store.GetContext(ctx, name)
 		if err != nil {
-			return exitError(ctx, ExitEncrypt, err, "failed to set key '%s' of '%s': %s", key, name, err)
+			return ctx, exitError(ctx, ExitEncrypt, err, "failed to set key '%s' of '%s': %s", key, name, err)
 		}
 		sec.SetPassword(password)
 		if err := s.Store.Set(sub.WithReason(ctx, "Generated password for YAML key"), name, sec); err != nil {
-			return exitError(ctx, ExitEncrypt, err, "failed to set key '%s' of '%s': %s", key, name, err)
+			return ctx, exitError(ctx, ExitEncrypt, err, "failed to set key '%s' of '%s': %s", key, name, err)
 		}
-		return nil
+		return ctx, nil
 	}
 
 	// generate a completely new secret
-	if err := s.Store.Set(sub.WithReason(ctx, "Generated Password"), name, secret.New(password, "")); err != nil {
-		return exitError(ctx, ExitEncrypt, err, "failed to create '%s': %s", name, err)
+	var err error
+	ctx, err = s.Store.SetContext(sub.WithReason(ctx, "Generated Password"), name, secret.New(password, ""))
+	if err != nil {
+		return ctx, exitError(ctx, ExitEncrypt, err, "failed to create '%s': %s", name, err)
 	}
-	return nil
+	return ctx, nil
 }

action/show.go
@@ -100,8 +100,11 @@ func (s *Action) showHandleOutput(ctx context.Context, name, key string, sec *se
 		case ctxutil.IsShowSafeContent(ctx) && !IsForce(ctx):
 			content = sec.Body()
 			if content == "" {
-				out.Yellow(ctx, "No safe content to display, you can force display with show -f.\nCopying password instead.")
-				return copyToClipboard(ctx, name, []byte(sec.Password()))
+				if ctxutil.IsAutoClip(ctx) {
+					out.Yellow(ctx, "No safe content to display, you can force display with show -f.\nCopying password instead.")
+					return copyToClipboard(ctx, name, []byte(sec.Password()))
+				}
+				return exitError(ctx, ExitNotFound, store.ErrNoBody, store.ErrNoBody.Error())
 			}
 		default:
 			buf, err := sec.Bytes()

commands.go
@@ -360,6 +360,10 @@ func getCommands(ctx context.Context, action *ap.Action, app *cli.App) []cli.Com
 			BashComplete: func(c *cli.Context) { action.Complete(ctx, c) },
 			Flags: []cli.Flag{
 				cli.BoolFlag{
+					Name:  "clip, c",
+					Usage: "Copy the generated password to the clipboard",
+				},
+				cli.BoolFlag{
 					Name:  "print, p",
 					Usage: "Print the generated password to the terminal",
 				},

config/config.go
@@ -37,6 +37,7 @@ func New() *Config {
 	return &Config{
 		Root: &StoreConfig{
 			AskForMore:    false,
+			AutoClip:      true,
 			AutoImport:    true,
 			AutoSync:      true,
 			ClipTimeout:   45,

config/context.go
@@ -31,5 +31,8 @@ func (c StoreConfig) WithContext(ctx context.Context) context.Context {
 	if !ctxutil.HasShowSafeContent(ctx) {
 		ctx = ctxutil.WithShowSafeContent(ctx, c.SafeContent)
 	}
+	if !ctxutil.HasAutoClip(ctx) {
+		ctx = ctxutil.WithAutoClip(ctx, c.AutoClip)
+	}
 	return ctx
 }

config/store_config.go
@@ -13,6 +13,7 @@ import (
 // StoreConfig is a per-store (root or mount) config
 type StoreConfig struct {
 	AskForMore     bool         `yaml:"askformore"`     // ask for more data on generate
+	AutoClip       bool         `yaml:"autoclip"`       // decide whether passwords are automatically copied or not
 	AutoImport     bool         `yaml:"autoimport"`     // import missing public keys w/o asking
 	AutoSync       bool         `yaml:"autosync"`       // push to git remote after commit, pull before push if necessary
 	ClipTimeout    int          `yaml:"cliptimeout"`    // clear clipboard after seconds
@@ -114,5 +115,5 @@ func (c *StoreConfig) SetConfigValue(key, value string) error {
 }
 
 func (c *StoreConfig) String() string {
-	return fmt.Sprintf("StoreConfig[AskForMore:%t,AutoImport:%t,AutoSync:%t,ClipTimeout:%d,EditRecipients:%t,NoColor:%t,NoConfirm:%t,NoPager:%t,Path:%s,SafeContent:%t,UseSymbols:%t]", c.AskForMore, c.AutoImport, c.AutoSync, c.ClipTimeout, c.EditRecipients, c.NoColor, c.NoConfirm, c.NoPager, c.Path, c.SafeContent, c.UseSymbols)
+	return fmt.Sprintf("StoreConfig[AskForMore:%t,AutoClip:%t,AutoImport:%t,AutoSync:%t,ClipTimeout:%d,EditRecipients:%t,NoColor:%t,NoConfirm:%t,NoPager:%t,Notifications:%t,Path:%s,SafeContent:%t,UseSymbols:%t]", c.AskForMore, c.AutoClip, c.AutoImport, c.AutoSync, c.ClipTimeout, c.EditRecipients, c.NoColor, c.NoConfirm, c.NoPager, c.Notifications, c.Path, c.SafeContent, c.UseSymbols)
 }

store/err.go
@@ -21,6 +21,8 @@ var (
 	ErrGitNoRemote = errors.Errorf("git has no remote origin")
 	// ErrGitNothingToCommit is returned if there are no staged changes
 	ErrGitNothingToCommit = errors.Errorf("git has nothing to commit")
+	// ErrNoBody is returned if a secret exists but has no content beyond a password
+	ErrNoBody = errors.Errorf("no safe content to display, you can force display with show -f")
 	// ErrNoPassword is returned is a secret exists but has no password, only a body
 	ErrNoPassword = errors.Errorf("no password to display")
 	// ErrYAMLNoMark is returned if a secret contains no valid YAML document marker

store/root/write.go
@@ -11,3 +11,9 @@ func (r *Store) Set(ctx context.Context, name string, sec *secret.Secret) error 
 	ctx, store, name := r.getStore(ctx, name)
 	return store.Set(ctx, name, sec)
 }
+
+// SetContext encodes and write the ciphertext of one entry to disk and propagate the context
+func (r *Store) SetContext(ctx context.Context, name string, sec *secret.Secret) (context.Context, error) {
+	ctx, store, name := r.getStore(ctx, name)
+	return ctx, store.Set(ctx, name, sec)
+}

utils/ctxutil/ctxutil.go
@@ -21,6 +21,7 @@ const (
 	ctxKeyNoColor
 	ctxKeyFuzzySearch
 	ctxKeyVerbose
+	ctxKeyAutoClip
 	ctxKeyNotifications
 	ctxKeyEditRecipients
 )
@@ -207,7 +208,7 @@ func IsNoPager(ctx context.Context) bool {
 	return bv
 }
 
-// WithShowSafeContent returns a context with the value for ask for more set
+// WithShowSafeContent returns a context with the value for ShowSafeContent set
 func WithShowSafeContent(ctx context.Context, bv bool) context.Context {
 	return context.WithValue(ctx, ctxKeyShowSafeContent, bv)
 }
@@ -218,7 +219,7 @@ func HasShowSafeContent(ctx context.Context) bool {
 	return ok
 }
 
-// IsShowSafeContent returns the value of ask for more or the default (false)
+// IsShowSafeContent returns the value of ShowSafeContent or the default (false)
 func IsShowSafeContent(ctx context.Context) bool {
 	bv, ok := ctx.Value(ctxKeyShowSafeContent).(bool)
 	if !ok {
@@ -234,7 +235,7 @@ func WithGitCommit(ctx context.Context, bv bool) context.Context {
 
 // HasGitCommit returns true if a value for GitCommit has been set in this context
 func HasGitCommit(ctx context.Context) bool {
-	_, ok := ctx.Value(ctxKeyShowSafeContent).(bool)
+	_, ok := ctx.Value(ctxKeyGitCommit).(bool)
 	return ok
 }
 
@@ -361,6 +362,26 @@ func HasNotifications(ctx context.Context) bool {
 // IsNotifications returns the value of Notifications or the default (true)
 func IsNotifications(ctx context.Context) bool {
 	bv, ok := ctx.Value(ctxKeyNotifications).(bool)
+	if !ok {
+		return true
+	}
+	return bv
+}
+
+// WithAutoClip returns a context with the value for AutoClip set
+func WithAutoClip(ctx context.Context, bv bool) context.Context {
+	return context.WithValue(ctx, ctxKeyAutoClip, bv)
+}
+
+// HasAutoClip returns true if a value for AutoClip has been set in this context
+func HasAutoClip(ctx context.Context) bool {
+	_, ok := ctx.Value(ctxKeyAutoClip).(bool)
+	return ok
+}
+
+// IsAutoClip returns the value of AutoClip or the default (true)
+func IsAutoClip(ctx context.Context) bool {
+	bv, ok := ctx.Value(ctxKeyAutoClip).(bool)
 	if !ok {
 		return true
 	}

action/config_test.go
@@ -39,6 +39,7 @@ func TestConfig(t *testing.T) {
 	assert.NoError(t, act.Config(ctx, c))
 	want := `root store config:
   askformore: false
+  autoclip: true
   autoimport: true
   autosync: true
   cliptimeout: 45
@@ -81,6 +82,7 @@ foo/nopager: false`
 	act.printConfigValues(ctx, "")
 	want = `root store config:
   askformore: false
+  autoclip: true
   autoimport: true
   autosync: true
   cliptimeout: 45
@@ -94,6 +96,7 @@ foo/nopager: false`
 	want += `  safecontent: false
   usesymbols: false
 mount 'foo' config:
+  autoclip: false
   autoimport: false
   autosync: false
   cliptimeout: 23
@@ -126,6 +129,7 @@ mount 'foo' config:
 	// action.ConfigComplete
 	act.ConfigComplete(c)
 	want = `askformore
+autoclip
 autoimport
 autosync
 cliptimeout

config/config_test.go
@@ -22,7 +22,7 @@ func TestNewConfig(t *testing.T) {
 	assert.Equal(t, backend.GPGCLI, cfg.Root.Path.Crypto)
 	assert.Equal(t, backend.GitCLI, cfg.Root.Path.RCS)
 	assert.Equal(t, backend.FS, cfg.Root.Path.Storage)
-	assert.Equal(t, "Config[Root:StoreConfig[AskForMore:false,AutoImport:true,AutoSync:true,ClipTimeout:45,EditRecipients:false,NoColor:false,NoConfirm:false,NoPager:false,Path:gpgcli-gitcli-fs+file:,SafeContent:false,UseSymbols:false],Mounts(),Version:]", cfg.String())
+	assert.Equal(t, "Config[Root:StoreConfig[AskForMore:false,AutoClip:true,AutoImport:true,AutoSync:true,ClipTimeout:45,EditRecipients:false,NoColor:false,NoConfirm:false,NoPager:false,Notifications:true,Path:gpgcli-gitcli-fs+file:,SafeContent:false,UseSymbols:false],Mounts(),Version:]", cfg.String())
 
 	cfg = nil
 	assert.Error(t, cfg.checkDefaults())
@@ -33,7 +33,7 @@ func TestNewConfig(t *testing.T) {
 	cfg.Mounts["foo"] = &StoreConfig{}
 	cfg.Mounts["bar"] = &StoreConfig{}
 	assert.NoError(t, cfg.checkDefaults())
-	assert.Equal(t, "Config[Root:StoreConfig[AskForMore:false,AutoImport:false,AutoSync:false,ClipTimeout:0,EditRecipients:false,NoColor:false,NoConfirm:false,NoPager:false,Path:gpgcli-gitcli-fs+file:,SafeContent:false,UseSymbols:false],Mounts(bar=>StoreConfig[AskForMore:false,AutoImport:false,AutoSync:false,ClipTimeout:0,EditRecipients:false,NoColor:false,NoConfirm:false,NoPager:false,Path:gpgcli-gitcli-fs+file:,SafeContent:false,UseSymbols:false]foo=>StoreConfig[AskForMore:false,AutoImport:false,AutoSync:false,ClipTimeout:0,EditRecipients:false,NoColor:false,NoConfirm:false,NoPager:false,Path:gpgcli-gitcli-fs+file:,SafeContent:false,UseSymbols:false]),Version:]", cfg.String())
+	assert.Equal(t, "Config[Root:StoreConfig[AskForMore:false,AutoClip:false,AutoImport:false,AutoSync:false,ClipTimeout:0,EditRecipients:false,NoColor:false,NoConfirm:false,NoPager:false,Notifications:false,Path:gpgcli-gitcli-fs+file:,SafeContent:false,UseSymbols:false],Mounts(bar=>StoreConfig[AskForMore:false,AutoClip:false,AutoImport:false,AutoSync:false,ClipTimeout:0,EditRecipients:false,NoColor:false,NoConfirm:false,NoPager:false,Notifications:false,Path:gpgcli-gitcli-fs+file:,SafeContent:false,UseSymbols:false]foo=>StoreConfig[AskForMore:false,AutoClip:false,AutoImport:false,AutoSync:false,ClipTimeout:0,EditRecipients:false,NoColor:false,NoConfirm:false,NoPager:false,Notifications:false,Path:gpgcli-gitcli-fs+file:,SafeContent:false,UseSymbols:false]),Version:]", cfg.String())
 }
 
 func TestSetConfigValue(t *testing.T) {

tests/show_test.go
@@ -33,8 +33,14 @@ func TestShow(t *testing.T) {
 	_, err = ts.run("show foo -force")
 	assert.NoError(t, err)
 
-	out, _ = ts.run("show fixed/secret")
-	assert.Contains(t, out, "No safe content to display, you can force display with show -f.\nCopying password instead.")
+	out, err = ts.run("show fixed/secret")
+	assert.NoError(t, err)
+	assert.Contains(t, out, "safe content to display, you can force display with show -f.\nCopying password instead.")
+
+	_, err = ts.run("config autoclip false")
+	assert.NoError(t, err)
+	_, err = ts.run("show fixed/secret")
+	assert.Error(t, err)
 
 	out, err = ts.run("show -f fixed/secret")
 	assert.NoError(t, err)
@@ -50,7 +56,7 @@ func TestShow(t *testing.T) {
 
 	out, err = ts.run("show fixed/twoliner -c")
 	assert.NoError(t, err)
-	assert.NotContains(t, out, "No safe content to display, you can force display with show -f")
+	assert.NotContains(t, out, "safe content to display")
 
 	_, err = ts.run("config safecontent false")
 	assert.NoError(t, err)

tests/tester.go
@@ -23,6 +23,7 @@ import (
 const (
 	gopassConfig = `root:
   askformore: false
+  autoclip: true
   autoimport: true
   autosync: false
   cliptimeout: 45