20dfe2c4faf50c70461c0d505c4ffdcc652f5a81
Dominik Schulz <dominik.schulz@justwatch.com>
2018-05-14 14:08:28 +0200

    Improve HIBPv2 API response handling

--- pkg/hibp/api/client.go
+++ pkg/hibp/api/client.go
@@ -54,7 +54,8 @@ func Lookup(ctx context.Context, shaSum string) (uint64, error) {
 			return fmt.Errorf("HTTP request failed: %s %s", resp.Status, body)
 		}
 
-		for _, line := range strings.Split(string(body), "\r\n") {
+		for _, line := range strings.Split(string(body), "\n") {
+			line = strings.TrimSpace(line)
 			if len(line) < 37 {
 				continue
 			}
--- pkg/hibp/api/client_test.go
+++ pkg/hibp/api/client_test.go
@@ -51,6 +51,44 @@ func TestLookup(t *testing.T) {
 	assert.Equal(t, uint64(0), count)
 }
 
+func TestLookupCR(t *testing.T) {
+	ctx := context.Background()
+
+	match := "match"
+	noMatch := "no match"
+	matchSum := sha1sum(match)
+	var matchCount uint64 = 324567
+
+	reqCnt := 0
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		reqCnt++
+		if reqCnt < 2 {
+			http.Error(w, "fake error", http.StatusInternalServerError)
+			return
+		}
+		if strings.TrimPrefix(r.URL.String(), "/range/") == matchSum[:5] {
+			fmt.Fprintf(w, matchSum[5:10]+":1\n")                            // invalid
+			fmt.Fprintf(w, matchSum[5:39]+":3234879\n")                      // invalid
+			fmt.Fprintf(w, matchSum[5:]+":\n")                               // invalid
+			fmt.Fprintf(w, matchSum[5:]+"\n")                                // invalid
+			fmt.Fprintf(w, fmt.Sprintf("%s:%d\n", matchSum[5:], matchCount)) // valid
+			return
+		}
+		http.Error(w, "not found", http.StatusNotFound)
+	}))
+	defer ts.Close()
+	URL = ts.URL
+
+	// test with one entry
+	count, err := Lookup(ctx, matchSum)
+	assert.NoError(t, err)
+	assert.Equal(t, matchCount, count)
+
+	// add another one
+	count, err = Lookup(ctx, sha1sum(noMatch))
+	assert.NoError(t, err)
+	assert.Equal(t, uint64(0), count)
+}
 func sha1sum(data string) string {
 	h := sha1.New()
 	_, _ = h.Write([]byte(data))
