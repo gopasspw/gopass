22e1bd4f0467fadfceca70edfcb12c1bd5fa734e
Dominik Schulz <dominik.schulz@gmail.com>
2018-05-23 10:12:42 +0200

    Add --nogit compat handler (#794)
    Fixes #792

--- pkg/action/init_test.go
+++ pkg/action/init_test.go
@@ -4,10 +4,12 @@ import (
 	"bytes"
 	"context"
 	"flag"
+	"fmt"
 	"os"
 	"path/filepath"
 	"testing"
 
+	"github.com/justwatchcom/gopass/pkg/backend"
 	"github.com/justwatchcom/gopass/pkg/ctxutil"
 	"github.com/justwatchcom/gopass/pkg/out"
 	"github.com/justwatchcom/gopass/tests/gptest"
@@ -50,4 +52,81 @@ func TestInit(t *testing.T) {
 	assert.NoError(t, os.Remove(filepath.Join(u.StoreDir(""), ".gpg-id")))
 	assert.Error(t, act.Initialized(ctx, c))
 	buf.Reset()
+}
+
+func TestInitParseContext(t *testing.T) {
+	buf := &bytes.Buffer{}
+	out.Stdout = buf
+	defer func() {
+		out.Stdout = os.Stdout
+	}()
+
+	for _, tc := range []struct {
+		name  string
+		args  []string
+		check func(context.Context) error
+	}{
+		{
+			name: "crypto xc",
+			args: []string{"--crypto=xc"},
+			check: func(ctx context.Context) error {
+				if backend.GetCryptoBackend(ctx) != backend.XC {
+					return fmt.Errorf("wrong backend")
+				}
+				return nil
+			},
+		},
+		{
+			name: "rcs noop",
+			args: []string{"--rcs=noop"},
+			check: func(ctx context.Context) error {
+				if backend.GetRCSBackend(ctx) != backend.Noop {
+					return fmt.Errorf("wrong backend")
+				}
+				return nil
+			},
+		},
+		{
+			name: "nogit",
+			args: []string{"--nogit"},
+			check: func(ctx context.Context) error {
+				if backend.GetRCSBackend(ctx) != backend.Noop {
+					return fmt.Errorf("wrong backend")
+				}
+				return nil
+			},
+		},
+		{
+			name: "default",
+			args: []string{},
+			check: func(ctx context.Context) error {
+				if backend.GetRCSBackend(ctx) != backend.GitCLI {
+					return fmt.Errorf("wrong backend")
+				}
+				return nil
+			},
+		},
+	} {
+		app := cli.NewApp()
+		fs := flag.NewFlagSet("default", flag.ContinueOnError)
+		sf := cli.StringFlag{
+			Name:  "crypto",
+			Usage: "crypto",
+		}
+		assert.NoError(t, sf.ApplyWithError(fs))
+		sf = cli.StringFlag{
+			Name:  "rcs",
+			Usage: "rcs",
+		}
+		assert.NoError(t, sf.ApplyWithError(fs))
+		bf := cli.BoolFlag{
+			Name:  "nogit",
+			Usage: "nogit",
+		}
+		assert.NoError(t, bf.ApplyWithError(fs))
+		assert.NoError(t, fs.Parse(tc.args), tc.name)
+		c := cli.NewContext(app, fs, nil)
+		assert.NoError(t, tc.check(initParseContext(context.Background(), c)), tc.name)
+		buf.Reset()
+	}
 }
