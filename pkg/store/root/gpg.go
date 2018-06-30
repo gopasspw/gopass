2415d2341124b3d3e44ca9893cf02accc2aa058f
Dominik Schulz <dominik.schulz@gmail.com>
2018-06-30 22:01:59 +0200

    Use valid crypto backend for key selection (#889)
    Fixes #888
    
    Signed-off-by: Dominik Schulz <dominik.schulz@gauner.org>

--- pkg/store/root/gpg.go
+++ pkg/store/root/gpg.go
@@ -4,12 +4,14 @@ import (
 	"context"
 
 	"github.com/gopasspw/gopass/pkg/backend"
+	"github.com/gopasspw/gopass/pkg/out"
 )
 
 // Crypto returns the crypto backend
 func (r *Store) Crypto(ctx context.Context, name string) backend.Crypto {
 	_, sub, _ := r.getStore(ctx, name)
 	if !sub.Valid() {
+		out.Debug(ctx, "Sub-Store not found for %s. Returning nil crypto backend", name)
 		return nil
 	}
 	return sub.Crypto()
