dec28c396069e6ef16e2b52467a6b4a0e70958a2
Cole Mickens <cole.mickens@gmail.com>
2018-11-11 14:10:30 -0800

    jsonapi: return totp in the api response to enable 2FA use in gopassbridge (#975)
    * otp: return specific error if totp entry missing
    
    Signed-off-by: Cole Mickens <cole.mickens@gmail.com>
    
    * jsonapi: return otp in secret data if available
    
    Signed-off-by: Cole Mickens <cole.mickens@gmail.com>

--- pkg/jsonapi/api_test.go
+++ pkg/jsonapi/api_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"encoding/binary"
+	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -13,6 +14,7 @@ import (
 
 	"github.com/gopasspw/gopass/pkg/backend"
 	"github.com/gopasspw/gopass/pkg/config"
+	"github.com/gopasspw/gopass/pkg/otp"
 	"github.com/gopasspw/gopass/pkg/store"
 	"github.com/gopasspw/gopass/pkg/store/root"
 	"github.com/gopasspw/gopass/pkg/store/secret"
@@ -125,7 +127,12 @@ func TestRespondMessageQuery(t *testing.T) {
 }
 
 func TestRespondMessageGetData(t *testing.T) {
+	totpSuffix := "//totp/github-fake-account?secret=rpna55555qyho42j"
+	totpURL := "otpauth:" + totpSuffix
+	totpSecret := secret.New("totp_are_cool", totpURL)
+
 	secrets := []storedSecret{
+		{[]string{"totp"}, totpSecret},
 		{[]string{"foo"}, secret.New("20", "hallo: welt")},
 		{[]string{"bar"}, secret.New("20", "---\nlogin: muh")},
 		{[]string{"complex"}, secret.New("20", `---
@@ -135,6 +142,12 @@ sub:
   subentry: 123
 `)},
 	}
+
+	totp, _, err := otp.Calculate(context.Background(), "_", totpSecret)
+	if err != nil {
+		assert.NoError(t, err)
+	}
+	expectedTotp := totp.OTP()
 
 	runRespondMessage(t,
 		`{"type":"getData","entry":"foo"}`,
@@ -148,6 +161,11 @@ sub:
 	runRespondMessage(t,
 		`{"type":"getData","entry":"complex"}`,
 		`{"login":"hallo","number":42,"sub":{"subentry":123}}`,
+		"", secrets)
+
+	runRespondMessage(t,
+		`{"type":"getData","entry":"totp"}`,
+		fmt.Sprintf(`{"current_totp":"%s","otpauth":"(.+)"}`, expectedTotp),
 		"", secrets)
 }
 
