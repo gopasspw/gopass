e3a60a85f855e94e3f77afadef764ff60471f139
Daniel Paquet <daniel.paquet@nokernel.com>
2018-12-05 08:21:42 -0500

    Fix without removing code
    Signed-off-by: Daniel Paquet <daniel.paquet@nokernel.com>

--- pkg/otp/otp.go
+++ pkg/otp/otp.go
@@ -34,11 +34,11 @@ func Calculate(ctx context.Context, name string, sec store.Secret) (twofactor.OT
 	// check yaml entry and fall back to password if we don't have one
 	label := name
 	secKey, err := sec.Value("totp")
-	/*if secKey == "" {
-		return nil, label, ErrNoTotpEntry
-	}*/
 	if err != nil {
 		secKey = sec.Password()
+	}
+	if secKey == "" {
+		return nil, label, ErrNoTotpEntry
 	}
 
 	if strings.HasPrefix(secKey, "otpauth://") {
