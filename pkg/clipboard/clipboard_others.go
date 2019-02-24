b4b8bce270baf248d89cecbd39c6f7523ef745c1
Dominik Schulz <dominik.schulz@gmail.com>
2019-02-24 22:01:19 +0100

    Improve killPrecedessors in unclip (#1074)
    The previous method was poorly optimized. This commit comes with a speedup
    on the order of two magnitudes.
    
    goos: linux
    goarch: amd64
    pkg: github.com/gopasspw/gopass/pkg/clipboard
    BenchmarkWalkProc-8            1        1265002845 ns/op
    BenchmarkListProc-8          100          17014944 ns/op
    PASS
    ok      github.com/gopasspw/gopass/pkg/clipboard        4.397s
    
    Fixes #923
    
    Signed-off-by: Dominik Schulz <dominik.schulz@gauner.org>

--- pkg/clipboard/clipboard_others.go
+++ pkg/clipboard/clipboard_others.go
@@ -10,10 +10,10 @@ import (
 	"io/ioutil"
 	"os"
 	"os/exec"
-	"path/filepath"
 	"strconv"
-	"strings"
 	"syscall"
+
+	ps "github.com/mitchellh/go-ps"
 )
 
 // clear will spwan a copy of gopass that waits in a detached background
@@ -35,56 +35,50 @@ func clear(ctx context.Context, content []byte, timeout int) error {
 	return cmd.Start()
 }
 
+func walkFn(pid int, killFn func(int)) {
+	// read the commandline for this process
+	cmdline, err := ioutil.ReadFile(fmt.Sprintf("/proc/%d/cmdline", pid))
+	if err != nil {
+		return
+	}
+	// compare the name of the binary and the first argument to avoid killing
+	// any unrelated (gopass) processes
+	args := bytes.Split(cmdline, []byte{0})
+	if len(args) < 2 {
+		return
+	}
+	// the commandline should start with "gopass"
+	if string(args[0]) != os.Args[0] {
+		return
+	}
+	// and have "unclip" as the first argument
+	if string(args[1]) != "unclip" {
+		return
+	}
+
+	killFn(pid)
+}
+
+func killProc(pid int) {
+	// err should be always nil, but just to be sure
+	proc, err := os.FindProcess(pid)
+	if err != nil {
+		return
+	}
+	// we ignore this error as we're going to return nil anyway
+	_ = proc.Kill()
+}
+
 // killPrecedessors will kill any previous "gopass unclip" invokations to avoid
 // erasing the clipboard prematurely in case the the same content is copied to
 // the clipboard repeately
 func killPrecedessors() error {
-	return filepath.Walk("/proc", func(path string, info os.FileInfo, err error) error {
-		// ignore any errors as they are most likely due to insufficient privileges
-		// (i.e. other users processes). We shouldn't get any errors for files we're
-		// intrested in on a pseudo filesystem
-		if err != nil {
-			return nil
-		}
-		// only looking for paths that follow this pattern:
-		// /proc/<pid>/status
-		if strings.Count(path, "/") != 3 {
-			return nil
-		}
-		if !strings.HasSuffix(path, "/status") {
-			return nil
-		}
-		// extract the numeric pid
-		pid, err := strconv.Atoi(path[6:strings.LastIndex(path, "/")])
-		if err != nil {
-			return nil
-		}
-		// read the commandline for this process
-		cmdline, err := ioutil.ReadFile(fmt.Sprintf("/proc/%d/cmdline", pid))
-		if err != nil {
-			return nil
-		}
-		// compare the name of the binary and the first argument to avoid killing
-		// any unrelated (gopass) processes
-		args := bytes.Split(cmdline, []byte{0})
-		if len(args) < 2 {
-			return nil
-		}
-		// the commandline should start with "gopass"
-		if string(args[0]) != os.Args[0] {
-			return nil
-		}
-		// and have "unclip" as the first argument
-		if string(args[1]) != "unclip" {
-			return nil
-		}
-		// err should be always nil, but just to be sure
-		proc, err := os.FindProcess(pid)
-		if err != nil {
-			return nil
-		}
-		// we ignore this error as we're going to return nil anyway
-		_ = proc.Kill()
-		return nil
-	})
+	procs, err := ps.Processes()
+	if err != nil {
+		return err
+	}
+	for _, proc := range procs {
+		walkFn(proc.Pid(), killProc)
+	}
+	return nil
 }
--- pkg/clipboard/clipboard_test.go
+++ pkg/clipboard/clipboard_test.go
@@ -3,10 +3,15 @@ package clipboard
 import (
 	"bytes"
 	"context"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
 	"testing"
 	"time"
 
 	"github.com/gopasspw/gopass/pkg/out"
+	ps "github.com/mitchellh/go-ps"
 
 	"github.com/atotto/clipboard"
 	"github.com/stretchr/testify/assert"
@@ -26,4 +31,38 @@ func TestClearClipboard(t *testing.T) {
 	ctx := context.Background()
 	assert.NoError(t, clear(ctx, []byte("bar"), 0))
 	time.Sleep(50 * time.Millisecond)
+}
+
+func BenchmarkWalkProc(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = filepath.Walk("/proc", func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return nil
+			}
+			if strings.Count(path, "/") != 3 {
+				return nil
+			}
+			if !strings.HasSuffix(path, "/status") {
+				return nil
+			}
+			pid, err := strconv.Atoi(path[6:strings.LastIndex(path, "/")])
+			if err != nil {
+				return nil
+			}
+			walkFn(pid, func(int) {})
+			return nil
+		})
+	}
+}
+
+func BenchmarkListProc(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		procs, err := ps.Processes()
+		if err != nil {
+			b.Fatalf("err: %s", err)
+		}
+		for _, proc := range procs {
+			walkFn(proc.Pid(), func(int) {})
+		}
+	}
 }
