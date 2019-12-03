b0bd068a16f7a0e5c87b03188c0dbcc79b918ada
acheron2302@protonmail.com <acheron2302@protonmail.com>
2019-12-03 17:25:46 +0700

    Update go.mod and fix completion error when download with go get
    Signed-off-by: acheron2302@protonmail.com <acheron2302@protonmail.com>

--- pkg/backend/crypto/xc.go
+++ pkg/backend/crypto/xc.go
@@ -2,4 +2,4 @@
 
 package crypto
 
-import _ "github.com/gopasspw/gopass/pkg/backend/crypto/xc" // registers XC backend if build tag xc is set
+import _ "github.com/gopass/gopass/pkg/backend/crypto/xc" // registers XC backend if build tag xc is set
