0985ba3a2001d649f0d13a7cb757ee95bd1224d7
Moritz Johner <moritz.johner@extern.dkbcodefactory.com>
2020-04-20 12:36:02 +0200

    fix: cross-platform tree listing
    sort.Strings() behaves differently on *nix/win with `/` and `\` characters

--- pkg/tree/simple/folder_test.go
+++ pkg/tree/simple/folder_test.go
@@ -1,28 +1,30 @@
 package simple
 
 import (
-	"github.com/fatih/color"
+	"path/filepath"
 	"sort"
 	"testing"
+
+	"github.com/fatih/color"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestFolder(t *testing.T) {
 	root := New("gopass")
-	assert.NoError(t, root.AddFile("foo/bar", "text/plain"))
+	assert.NoError(t, root.AddFile(filepath.Join("foo", "bar"), "text/plain"))
 	assert.NoError(t, root.AddTemplate("foo"))
-	assert.NoError(t, root.AddFile("foo/baz.b64", "application/octet-stream"))
-	assert.NoError(t, root.AddFile("foo/zab.yml", "text/yaml"))
+	assert.NoError(t, root.AddFile(filepath.Join("foo", "baz.b64"), "application/octet-stream"))
+	assert.NoError(t, root.AddFile(filepath.Join("foo", "zab.yml"), "text/yaml"))
 	assert.Equal(t, 3, root.Len())
 
 	// test list
 	lst := root.List(0)
 	sort.Strings(lst)
 	wants := []string{
-		"foo/bar",
-		"foo/baz.b64",
-		"foo/zab.yml",
+		filepath.Join("foo", "bar"),
+		filepath.Join("foo", "baz.b64"),
+		filepath.Join("foo", "zab.yml"),
 	}
 	assert.Equal(t, wants, lst)
 
@@ -42,36 +44,36 @@ func TestFolder(t *testing.T) {
 
 	// test list 1
 	root = New("gopass")
-	assert.NoError(t, root.AddFile("zab/foozen", "text/plain"))
-	assert.NoError(t, root.AddFile("zab/foo/bar", "text/plain"))
-	assert.NoError(t, root.AddFile("zab2/foo/baz", "text/plain"))
-	assert.NoError(t, root.AddFile("zab2/foo/zen/baz", "text/plain"))
+	assert.NoError(t, root.AddFile(filepath.Join("zab", "foozen"), "text/plain"))
+	assert.NoError(t, root.AddFile(filepath.Join("zab", "foo", "bar"), "text/plain"))
+	assert.NoError(t, root.AddFile(filepath.Join("zab2", "foo", "baz"), "text/plain"))
+	assert.NoError(t, root.AddFile(filepath.Join("zab2", "foo", "zen", "baz"), "text/plain"))
 
 	lst = root.List(1)
 	sort.Strings(lst)
 	wants = []string{
-		"zab/foozen",
+		filepath.Join("zab", "foozen"),
 	}
 	assert.Equal(t, wants, lst)
 
 	// test folders
 	lst = root.ListFolders(0)
 	wants = []string{
-		"zab",
-		"zab/foo",
-		"zab2",
-		"zab2/foo",
-		"zab2/foo/zen",
+		filepath.Join("zab"),
+		filepath.Join("zab", "foo"),
+		filepath.Join("zab2"),
+		filepath.Join("zab2", "foo"),
+		filepath.Join("zab2", "foo", "zen"),
 	}
 	assert.Equal(t, wants, lst)
 
 	// test folders maxDepth = 1
 	lst = root.ListFolders(1)
 	wants = []string{
-		"zab",
-		"zab/foo",
-		"zab2",
-		"zab2/foo",
+		filepath.Join("zab"),
+		filepath.Join("zab", "foo"),
+		filepath.Join("zab2"),
+		filepath.Join("zab2", "foo"),
 	}
 	assert.Equal(t, wants, lst)
 
