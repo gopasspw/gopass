e3ab4c3e276ac6b035b326e60652a4f1c8eeb693
worldofpeace <worldofpeace@protonmail.ch>
2020-04-24 11:43:38 -0400

    Singular they (#1258)
    Co-Authored-By: Pharb <Pharb@users.noreply.github.com>
    Signed-off-by: worldofpeace <worldofpeace@protonmail.ch>
    
    Co-authored-by: Pharb <Pharb@users.noreply.github.com>

--- pkg/store/store.go
+++ pkg/store/store.go
@@ -11,8 +11,8 @@ import (
 // RecipientCallback is a callback to verify the list of recipients
 type RecipientCallback func(context.Context, string, []string) ([]string, error)
 
-// ImportCallback is a callback to ask the user if he wants to import
-// a certain recipients public key into his keystore
+// ImportCallback is a callback to ask the user if they want to import
+// a certain recipients public key into their keystore
 type ImportCallback func(context.Context, string, []string) bool
 
 // FsckCallback is a callback to ask the user to confirm certain fsck
