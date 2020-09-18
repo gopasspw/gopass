854b203a99c6f9fafc6d6543afdd4ffbc51e74cf
Ilya Glotov <ilyaglow@users.noreply.github.com>
2020-09-18 21:27:53 +0300

    Create cache folder if doesn't exist (#1580)
    RELEASE_NOTES=[BUGFIX] Create cache folder if doesn't exist. Relevant
    for darwin that doesn't have ~/.cache folder by default.
    
    Signed-off-by: Ilya Glotov <contact@ilyaglotov.com>

--- internal/notify/icon.go
+++ internal/notify/icon.go
@@ -12,7 +12,14 @@ import (
 )
 
 func iconURI() string {
-	iconFN := filepath.Join(appdir.UserCache(), "gopass-logo-small.png")
+	userCache := appdir.UserCache()
+	if !fsutil.IsDir(userCache) {
+		if err := os.MkdirAll(userCache, 0755); err != nil {
+			return ""
+		}
+	}
+
+	iconFN := filepath.Join(userCache, "gopass-logo-small.png")
 	if !fsutil.IsFile(iconFN) {
 		fh, err := os.OpenFile(iconFN, os.O_WRONLY|os.O_CREATE, 0644)
 		if err != nil {
