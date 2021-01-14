2b4834b784f0b6ab8f991c50dc6ca2900900ba50
Raphaël Pinson <raphael.pinson@camptocamp.com>
2021-01-14 17:37:08 +0100

    Simplify implementation

--- cmd/gopass-summon-provider/summon-provider.go
+++ cmd/gopass-summon-provider/summon-provider.go
@@ -5,7 +5,6 @@ import (
 	"io"
 	"os"
 
-	"github.com/gopasspw/gopass/internal/action"
 	"github.com/gopasspw/gopass/pkg/ctxutil"
 	"github.com/gopasspw/gopass/pkg/gopass"
 	"github.com/urfave/cli/v2"
@@ -25,24 +24,20 @@ func (s *gc) Get(c *cli.Context) error {
 	ctx := ctxutil.WithGlobalFlags(c)
 	ctx = ctxutil.WithNoNetwork(ctx, true)
 	path := c.Args().Get(0)
-	key := c.Args().Get(1)
-	if key != "" {
-		ctx = action.WithKey(ctx, key)
-	}
 	secret, err := s.gp.Get(ctx, path, "latest")
 	if err != nil {
 		return err
 	}
 
-	if key == "" {
-		fmt.Fprintln(Stdout, secret.Password())
-		return nil
+	if key := c.Args().Get(1); key != "" {
+		if val, found := secret.Get(key); found {
+			fmt.Fprintln(Stdout, val)
+			return
+		}
+		fmt.Fprintln(Stdout, "Error getting subkey")
+		return
 	}
 
-	val, found := secret.Get(key)
-	if found {
-		fmt.Fprintln(Stdout, val)
-	}
-
+	fmt.Fprintln(Stdout, secret.Password())
 	return nil
 }
