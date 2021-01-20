1327ec1e78cb694615ca468b353e0c4f1cabd5e3
Dominik Schulz <dominik.schulz@gauner.org>
2021-01-20 10:44:26 +0100

    Use RSA release signing key (#1735)
    ECC has compatability issues so falling back to a plain old RSA key for
    automatic release signing.
    
    RELEASE_NOTES=n/a
    
    Signed-off-by: Dominik Schulz <dominik.schulz@gauner.org>

--- internal/updater/verify_test.go
+++ internal/updater/verify_test.go
@@ -6,15 +6,21 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// gpg -u 0x21FF4D8F35D1AEE5 --armor --output testdata.sig --detach-sign testdata
+// gpg -u 0x2F752B2CA00248FC --armor --output testdata.sig --detach-sign testdata
 var testSignature = []byte(`
 -----BEGIN PGP SIGNATURE-----
 
-iLgEABMKAB0WIQTiWnWvE2Kw5vo0dzgh/02PNdGu5QUCYAcgywAKCRAh/02PNdGu
-5TVbAgkBPO1k24iHSVrtfz0Mdy8RQbXxr50Y+I2PpL1Ai2e9f+yrUnmENnmzrlom
-/vhR47sCH0KEjJ0xVKsfCgkX9Tv/jZICBjv0s0GoAFkeM5mCSHTYfbZZSjFPnxR9
-A3ELxJxspsRt1awBSF79yPrJnuZIWMz81G4JhBBqbCpNM85982uSqXqo
-=vhCd
+iQGzBAABCgAdFiEEeXE+gccft5Z7UYXQL3UrLKACSPwFAmAH4zYACgkQL3UrLKAC
+SPzcbgv9GwH+bxGJ2IwPPm2IsCCpZyef4Sa7PK8O/4lbEEDcpinap6LZvxzga+xb
+8pLV/lFj/QXkxQM3qJbdongkiBu5A77FUmgUi2j1pmK0g9ExF9mYKE858zBBcWIj
+P+NyVXQ4OIYmpmTvBa9h4Fbtb3R9J0gXdTh9v/8txAkKNEAWH8C+4lp+MHUmHC/N
+O/dwoYF+K7eFlFIei/E0+T2tT2TPgUX3DIxbBqucG9ZERBz6SfjJSjzpQ4R9jQvs
+VuUjbTpzmg1elY1aUN41VaiMHY4QGgDILwrs8Y63pRY65Yrox9wAYwxqxlpFbcHT
+5FZTJi2W1fKDecrewD7SJ4iEvXawa0DuWZp1bS5rQyk9OJTHudzYAbQuDece6S3E
+H5n6eSuBp+fooGijs56IbHQxTclHdRpLNCDRCMf/0vIAtnLy2mcMvIiUCzMolQtx
+umpKYk4AOrn1Q56ChcLVx7PIKu618UYq37Tnn7hZqjxL1mTDKcmhU1gPflbLRXiQ
+WubSpxPm
+=xTH+
 -----END PGP SIGNATURE-----
 `)
 
