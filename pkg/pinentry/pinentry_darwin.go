2b2c605b584cbfa6d56167f4b492c0db7698b4ac
Dominik Schulz <dominik.schulz@gauner.org>
2021-02-02 19:36:20 +0100

    Use gpgconf(1) to locate the gpg binary (#1758)
    Attempt to use gpgconf(1) for locating the path to the GPG binary.
    
    Fixes #1757
    
    RELEASE_NOTES=[ENHANCEMENT] Use gpgconf to the the gpg binary
    
    Signed-off-by: Dominik Schulz <dominik.schulz@gauner.org>

--- pkg/pinentry/pinentry_darwin.go
+++ pkg/pinentry/pinentry_darwin.go
@@ -2,7 +2,12 @@
 
 package pinentry
 
+import "github.com/gopasspw/gopass/pkg/pinentry/gpgconf"
+
 // GetBinary always returns pinentry-mac
 func GetBinary() string {
+	if p, err := gpgconf.Path("pinentry"); err == nil && p != "" {
+		return p
+	}
 	return "pinentry-mac"
 }
--- pkg/pinentry/pinentry_windows.go
+++ pkg/pinentry/pinentry_windows.go
@@ -2,7 +2,12 @@
 
 package pinentry
 
+import "github.com/gopasspw/gopass/pkg/pinentry/gpgconf"
+
 // GetBinary always returns pinentry.exe
 func GetBinary() string {
+	if p, err := gpgconf.Path("pinentry"); err == nil && p != "" {
+		return p
+	}
 	return "pinentry.exe"
 }
