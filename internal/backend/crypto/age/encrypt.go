36c333912c58c870d06f47c9793e4f98736e9f8d
Dominik Schulz <mail@dschulz.eu>
2022-01-16 21:54:14 +0100

    save

--- internal/backend/crypto/age/encrypt.go
+++ internal/backend/crypto/age/encrypt.go
@@ -29,27 +29,6 @@ func (a *Age) Encrypt(ctx context.Context, plaintext []byte, recipients []string
 	return a.encrypt(plaintext, recp...)
 }
 
-// dedupe the recipients, only works for native age recipients.
-func dedupe(recp []age.Recipient) []age.Recipient {
-	out := make([]age.Recipient, 0, len(recp))
-	set := make(map[string]age.Recipient, len(recp))
-	for _, r := range recp {
-		k, ok := r.(fmt.Stringer)
-		// handle non-native recipients.
-		if !ok {
-			out = append(out, r)
-			continue
-		}
-		set[k.String()] = r
-	}
-
-	for _, r := range set {
-		out = append(out, r)
-	}
-	debug.Log("in: %+v - out: %+v", recp, out)
-	return out
-}
-
 func (a *Age) encrypt(plaintext []byte, recp ...age.Recipient) ([]byte, error) {
 	out := &bytes.Buffer{}
 	w, err := age.Encrypt(out, recp...)
@@ -77,10 +56,46 @@ func (a *Age) encryptFile(ctx context.Context, filename string, plaintext []byte
 	if err != nil {
 		return err
 	}
-	buf, err := a.encrypt(plaintext, id)
+
+	fh, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		return err
+	}
+	defer fh.Close()
+
+	w, err := age.Encrypt(fh, id)
+	if err != nil {
+		return err
+	}
+	defer w.Close()
+
+	n, err := w.Write(plaintext)
 	if err != nil {
 		return err
 	}
 
-	return os.WriteFile(filename, buf, 0600)
+	debug.Log("Encrypted %d bytes of plaintext to %d bytes of ciphertext for %q", len(plaintext), n, filename)
+
+	return nil
+}
+
+// dedupe the recipients, only works for native age recipients.
+func dedupe(recp []age.Recipient) []age.Recipient {
+	out := make([]age.Recipient, 0, len(recp))
+	set := make(map[string]age.Recipient, len(recp))
+	for _, r := range recp {
+		k, ok := r.(fmt.Stringer)
+		// handle non-native recipients.
+		if !ok {
+			out = append(out, r)
+			continue
+		}
+		set[k.String()] = r
+	}
+
+	for _, r := range set {
+		out = append(out, r)
+	}
+	debug.Log("in: %+v - out: %+v", recp, out)
+	return out
 }
