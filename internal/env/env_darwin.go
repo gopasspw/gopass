23bf95966628196588e54a16eccee13b78d8cb65
Dominik Schulz <mail@dschulz.eu>
2022-02-26 14:56:04 +0100

    Check org.gpgtools.common UseKeychain on MacOS
    RELEASE_NOTES=[ENHANCENMENT] Check for MacOS Keychain storing the GPG
    passphrase.
    
    Fixes #2137
    
    Signed-off-by: Dominik Schulz <dominik.schulz@gauner.org>

---
+++ internal/env/env_darwin.go
@@ -0,0 +1,42 @@
+//go:build darwin
+// +build darwin
+
+package env
+
+import (
+	"bytes"
+	"context"
+	"io"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+var (
+	// Stdin is exported for tests.
+	Stdin io.Reader = os.Stdin
+	// Stderr is exported for tests.
+	Stderr io.Writer = os.Stderr
+)
+
+func Check(ctx context.Context) (string, error) {
+	buf := &bytes.Buffer{}
+
+	cmd := exec.CommandContext(ctx, "defaults", "read", "org.gpgtools.common", "UseKeychain")
+	cmd.Stdin = Stdin
+	cmd.Stdout = buf
+	cmd.Stderr = Stderr
+
+	if err := cmd.Run(); err != nil {
+		return "", err
+	}
+
+	// if the keychain is not used, we can skip the rest
+	if strings.ToUpper(strings.TrimSpace(buf.String())) == "NO" {
+		return "", nil
+	}
+
+	// gpg uses the keychain to store the passphrase, warn once in a while that users
+	// might want to change that because it's not secure.
+	return "pinentry-mac will use the MacOS Keychain to store your passphrase indefinitely. Consider running 'defaults write org.gpgtools.common UseKeychain NO' to disable that.", nil
+}
---
+++ internal/env/env_others.go
@@ -0,0 +1,11 @@
+//go:build !darwin
+// +build !darwin
+
+package env
+
+import "context"
+
+// Check does nothing on these OSes, yet.
+func Check(ctx context.Context) (string, error) {
+	return "", nil
+}
