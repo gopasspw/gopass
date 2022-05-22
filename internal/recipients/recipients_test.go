7ee5b1384b35bbfb1ad3d4b20cd054c1615e2b1b
Dominik Schulz <dominik.schulz@gauner.org>
2022-05-22 10:00:55 +0200

    Handle Unix, Windows and Mac line endings in recipients parsing (#2221)
    Fixes #2220
    
    RELEASE_NOTES=[BUGFIX] Handle different line breaks in recipient
    handling.
    
    Signed-off-by: Dominik Schulz <dominik.schulz@gauner.org>

--- internal/recipients/recipients_test.go
+++ internal/recipients/recipients_test.go
@@ -1,9 +1,49 @@
 package recipients
 
-import "testing"
+import (
+	"sort"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestMarshal(t *testing.T) {
 	t.Parallel()
 
 	t.Skip("implement this")
+}
+
+func TestUnmarshal(t *testing.T) {
+	t.Parallel()
+
+	for _, tc := range []struct {
+		in   string
+		want []string
+	}{
+		{
+			in:   "foo@bar.com",
+			want: []string{"foo@bar.com"},
+		},
+		{
+			in:   "foo@bar.com\nbaz@bar.com\n",
+			want: []string{"baz@bar.com", "foo@bar.com"},
+		},
+		{
+			in:   "foo@bar.com\r\nbaz@bar.com\r\n",
+			want: []string{"baz@bar.com", "foo@bar.com"},
+		},
+		{
+			in:   "foo@bar.com\rbaz@bar.com\r",
+			want: []string{"baz@bar.com", "foo@bar.com"},
+		},
+	} {
+		tc := tc
+		t.Run(tc.in, func(t *testing.T) {
+			t.Parallel()
+
+			got := Unmarshal([]byte(tc.in))
+			sort.Strings(got)
+			assert.Equal(t, tc.want, got, tc.in)
+		})
+	}
 }
