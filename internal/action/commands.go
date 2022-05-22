de099d920271c0b3891c1ac47296738f2145d249
FG <git@dotcs.me>
2022-05-22 10:10:26 +0200

    Add flag '--keep-case' to env subcommend (#2226)
    * Add flag '--keep-capitalization' to env subcommend
    
    By adding this boolean flag to the `gopass env` cmd, the original
    capitalization of the secret is maintained.
    This fixes #2225.
    
    RELEASE_NOTES=[ENHANCEMENT] Add flag to keep env variable capitalization
    in `gopass env` subcmd
    
    Signed-off-by: dotcs <git@dotcs.me>
    
    * Rename CLI flag from 'keep-capitalization' to 'keep-case'
    
    Signed-off-by: dotcs <git@dotcs.me>

--- internal/action/commands.go
+++ internal/action/commands.go
@@ -300,6 +300,14 @@ func (s *Action) GetCommands() []*cli.Command {
 			Action:       s.Env,
 			BashComplete: s.Complete,
 			Hidden:       true,
+			Flags: []cli.Flag{
+				&cli.BoolFlag{
+					Name:    "keep-case",
+					Aliases: []string{"kc"},
+					Value:   false,
+					Usage:   "Do not capitalize the environment variable and instead retain the original capitalization",
+				},
+			},
 		},
 		{
 			Name:      "find",
--- internal/action/env_test.go
+++ internal/action/env_test.go
@@ -48,6 +48,41 @@ func TestEnvLeafHappyPath(t *testing.T) { //nolint:paralleltest
 	assert.Contains(t, buf.String(), fmt.Sprintf("BAZ=%s\n", pw))
 }
 
+func TestEnvLeafHappyPathKeepCase(t *testing.T) { //nolint:paralleltest
+	u := gptest.NewUnitTester(t)
+	defer u.Remove()
+
+	ctx := context.Background()
+	ctx = ctxutil.WithAlwaysYes(ctx, true)
+	ctx = ctxutil.WithTerminal(ctx, false)
+	act, err := newMock(ctx, u)
+	require.NoError(t, err)
+	require.NotNil(t, act)
+
+	buf := &bytes.Buffer{}
+	out.Stdout = buf
+	out.Stderr = buf
+	stdout = buf
+	defer func() {
+		out.Stdout = os.Stdout
+		out.Stderr = os.Stderr
+		stdout = os.Stdout
+	}()
+
+	// Command-line would be: "gopass env --keep-case BaZ env", where
+	// "foo" is an existing secret with value "secret". We expect to see the
+	// key/value in the output of the /usr/bin/env utility in the form
+	// "BaZ=secret".
+	pw := pwgen.GeneratePassword(24, false)
+	assert.NoError(t, act.insertStdin(ctx, "BaZ", []byte(pw), false))
+	buf.Reset()
+
+	flags := make(map[string]string, 1)
+	flags["keep-case"] = "true"
+	assert.NoError(t, act.Env(gptest.CliCtxWithFlags(ctx, t, flags, "BaZ", "env")))
+	assert.Contains(t, buf.String(), fmt.Sprintf("BaZ=%s\n", pw))
+}
+
 func TestEnvSecretNotFound(t *testing.T) { //nolint:paralleltest
 	u := gptest.NewUnitTester(t)
 	defer u.Remove()
