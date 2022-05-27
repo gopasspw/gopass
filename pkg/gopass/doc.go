86bdd4159b0728569c756109838ef60414488012
Dominik Schulz <mail@dschulz.eu>
2022-05-27 20:51:20 +0200

    Add API examples
    RELEASE_NOTES=n/a
    
    Signed-off-by: Dominik Schulz <dominik.schulz@gauner.org>

--- pkg/gopass/doc.go
+++ pkg/gopass/doc.go
@@ -6,4 +6,10 @@
 // note that bug reports will be silently ignored and the API WILL CHANGE
 // WITHOUT NOTICE until this note is gone.
 //
+// If you want to try it anyway please look at the following examples:
+// * https://github.com/gopasspw/gopass-hibp
+// * https://github.com/gopasspw/gopass-jsonapi
+// * https://github.com/gopasspw/git-credential-gopass
+// * https://github.com/gopasspw/gopass-summon-provider
+//
 package gopass
