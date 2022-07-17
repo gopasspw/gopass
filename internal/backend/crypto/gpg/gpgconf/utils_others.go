3206e377e3100ac8ab4033cae48906d2d0eecaf6
Dominik Schulz <dominik.schulz@gauner.org>
2022-07-17 17:32:06 +0200

    Fix linter issues
    RELEASE_NOTES=n/a
    
    Signed-off-by: Dominik Schulz <dominik.schulz@gauner.org>

--- internal/backend/crypto/gpg/gpgconf/utils_others.go
+++ internal/backend/crypto/gpg/gpgconf/utils_others.go
@@ -16,6 +16,7 @@ func TTY() string {
 	if err != nil {
 		return ""
 	}
+
 	return string(out)
 }
 
--- internal/notify/notify_darwin.go
+++ internal/notify/notify_darwin.go
@@ -21,7 +21,7 @@ var (
 	execLookPath = exec.LookPath
 )
 
-// Notify displays a desktop notification using osascript
+// Notify displays a desktop notification using osascript.
 func Notify(ctx context.Context, subj, msg string) error {
 	if os.Getenv("GOPASS_NO_NOTIFY") != "" || !ctxutil.IsNotifications(ctx) {
 		return nil
@@ -32,35 +32,39 @@ func Notify(ctx context.Context, subj, msg string) error {
 	if tn {
 		return tnNotification(msg, subj)
 	}
+
 	return osaNotification(msg, subj)
 }
 
-// display notification with osascript
+// display notification with osascript.
 func osaNotification(msg string, subj string) error {
 	_, err := executableExists(osascript)
 	if err != nil {
 		return err
 	}
 	args := []string{"-e", `display notification "` + msg + `" with title "` + subj + `"`}
+
 	return execNotification(osascript, args)
 }
 
-// exec notification program with passed arguments
+// exec notification program with passed arguments.
 func execNotification(executable string, args []string) error {
 	return execCommand(executable, args...).Start()
 }
 
-// display notification with terminal-notifier
+// display notification with terminal-notifier.
 func tnNotification(msg string, subj string) error {
 	arguments := []string{"-title", "Gopass", "-message", msg, "-subtitle", subj, "-appIcon", iconURI()}
+
 	return execNotification(terminalNotifier, arguments)
 }
 
-// check if executable exists
+// check if executable exists.
 func executableExists(executable string) (bool, error) {
 	_, err := execLookPath(executable)
 	if err != nil {
 		return false, err
 	}
+
 	return true, nil
 }
