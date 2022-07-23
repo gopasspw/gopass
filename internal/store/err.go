5da8099e8bbca315e81a7b4f7c7453197b6cb482
Dominik Schulz <dominik.schulz@gauner.org>
2022-07-23 09:31:02 +0200

    Link to FAQ for empty secret errors (#2286)
    RELEASE_NOTES=n/a
    
    Signed-off-by: Dominik Schulz <dominik.schulz@gauner.org>

--- internal/store/err.go
+++ internal/store/err.go
@@ -22,7 +22,8 @@ var (
 	// ErrGitNothingToCommit is returned if there are no staged changes.
 	ErrGitNothingToCommit = fmt.Errorf("git has nothing to commit")
 	// ErrEmptySecret is returned if a secret exists but has no content.
-	ErrEmptySecret = fmt.Errorf("empty secret")
+	// TODO: Replace with a shorter link to the secret
+	ErrEmptySecret = fmt.Errorf("empty secret. see https://github.com/gopasspw/gopass/blob/master/docs/faq.md#empty-secret")
 	// ErrNoBody is returned if a secret exists but has no content beyond a password.
 	ErrNoBody = fmt.Errorf("no safe content to display, you can force display with -f")
 	// ErrNoPassword is returned is a secret exists but has no password, only a body.
