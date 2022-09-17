67e85c29e19298e17a84d1d91995084852feb49f
Dominik Schulz <mail@dschulz.eu>
2022-09-17 18:42:44 +0200

    Do not ignore symlinks when listing store contents
    Fixes #2173
    
    RELEASE_NOTES=[BUGFIX] Do not ignore symlinks when listing
    
    Signed-off-by: Dominik Schulz <dominik.schulz@gauner.org>

---
+++ internal/backend/storage/fs/walk.go
@@ -0,0 +1,59 @@
+package fs
+
+import (
+	"io/fs"
+	"os"
+	"path/filepath"
+
+	"github.com/gopasspw/gopass/pkg/debug"
+)
+
+func walkSymlinks(path string, walkFn filepath.WalkFunc) error {
+	w := &walker{
+		seen: make(map[string]bool, 100),
+	}
+
+	return w.walk(path, path, walkFn)
+}
+
+type walker struct {
+	seen map[string]bool
+}
+
+func (w *walker) walk(filename, linkDir string, walkFn filepath.WalkFunc) error {
+	sWalkFn := func(path string, info fs.FileInfo, err error) error {
+		fname, err := filepath.Rel(filename, path)
+		if err != nil {
+			return err
+		}
+		path = filepath.Join(linkDir, fname)
+
+		if info.Mode()&fs.ModeSymlink == fs.ModeSymlink {
+			destPath, err := filepath.EvalSymlinks(path)
+			if err != nil {
+				return err
+			}
+
+			// avoid loops
+			if w.seen[destPath] {
+				debug.Log("Symlink loop detected at %s!", destPath)
+
+				return nil
+			}
+			w.seen[destPath] = true
+
+			destInfo, err := os.Lstat(destPath)
+			if err != nil {
+				return walkFn(path, destInfo, err)
+			}
+
+			if destInfo.IsDir() {
+				return w.walk(destPath, path, walkFn)
+			}
+		}
+
+		return walkFn(path, info, err)
+	}
+
+	return filepath.Walk(filename, sWalkFn)
+}
