192c32e5a6ae9e0c7d1456d5f14ed211a0963db8
Dominik Schulz <mail@dschulz.eu>
2022-12-03 21:06:59 +0100

    Fix symlink handling in walk
    Fixes #2402
    
    RELEASE_NOTES=[BUGFIX] Fix symlink deduplication.
    
    Signed-off-by: Dominik Schulz <dominik.schulz@gauner.org>

--- internal/backend/storage/fs/walk.go
+++ internal/backend/storage/fs/walk.go
@@ -4,23 +4,13 @@ import (
 	"io/fs"
 	"os"
 	"path/filepath"
-
-	"github.com/gopasspw/gopass/pkg/debug"
 )
 
 func walkSymlinks(path string, walkFn filepath.WalkFunc) error {
-	w := &walker{
-		seen: map[string]bool{},
-	}
-
-	return w.walk(path, path, walkFn)
+	return walk(path, path, walkFn)
 }
 
-type walker struct {
-	seen map[string]bool
-}
-
-func (w *walker) walk(filename, linkDir string, walkFn filepath.WalkFunc) error {
+func walk(filename, linkDir string, walkFn filepath.WalkFunc) error {
 	sWalkFn := func(path string, info fs.FileInfo, err error) error {
 		fname, err := filepath.Rel(filename, path)
 		if err != nil {
@@ -28,28 +18,24 @@ func (w *walker) walk(filename, linkDir string, walkFn filepath.WalkFunc) error 
 		}
 		path = filepath.Join(linkDir, fname)
 
-		if info.Mode()&fs.ModeSymlink == fs.ModeSymlink {
-			destPath, err := filepath.EvalSymlinks(path)
-			if err != nil {
-				return err
-			}
+		// handle non-symlinks
+		if info.Mode()&fs.ModeSymlink != fs.ModeSymlink {
+			return walkFn(path, info, err)
+		}
 
-			// avoid loops
-			if w.seen[destPath] {
-				debug.Log("Symlink loop detected at %s!", destPath)
+		// handle symlinks
+		destPath, err := filepath.EvalSymlinks(path)
+		if err != nil {
+			return err
+		}
 
-				return nil
-			}
-			w.seen[destPath] = true
+		destInfo, err := os.Lstat(destPath)
+		if err != nil {
+			return walkFn(path, destInfo, err)
+		}
 
-			destInfo, err := os.Lstat(destPath)
-			if err != nil {
-				return walkFn(path, destInfo, err)
-			}
-
-			if destInfo.IsDir() {
-				return w.walk(destPath, path, walkFn)
-			}
+		if destInfo.IsDir() {
+			return walk(destPath, path, walkFn)
 		}
 
 		return walkFn(path, info, err)
