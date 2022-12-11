gopasspw/gopass

commit f58454452fae01c605705a0da9f73ee8d145ed7c

Increase test coverage (#2461)
RELEASE_NOTES=n/a

Signed-off-by: Dominik Schulz <dominik.schulz@gauner.org>

Signed-off-by: Dominik Schulz <dominik.schulz@gauner.org>

pkg/debug/debug.go
@@ -52,6 +52,8 @@ func initDebug() bool {
 func initDebugLogger() {
 	debugfile := os.Getenv("GOPASS_DEBUG_LOG")
 	if debugfile == "" {
+		opts.logger = log.New(os.Stderr, "", log.Ldate|log.Lmicroseconds)
+
 		return
 	}
 

pkg/debug/debug_test.go
@@ -1,8 +1,14 @@
 package debug
 
 import (
+	"bytes"
+	"io/ioutil"
 	"os"
+	"path/filepath"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func BenchmarkLogging(b *testing.B) {
@@ -23,4 +29,102 @@ func BenchmarkNoLogging(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		Log("string")
 	}
+}
+
+// can not import out.Secret.
+type testSecret string
+
+func (t testSecret) SafeStr() string {
+	return "(elided)"
+}
+
+type testShort string
+
+func (t testShort) Str() string {
+	return "shorter"
+}
+
+func TestDebug(t *testing.T) {
+	td := t.TempDir()
+	t.Cleanup(func() {
+		initDebug()
+	})
+
+	fn := filepath.Join(td, "gopass.log")
+	t.Setenv("GOPASS_DEBUG_LOG", fn)
+
+	// it's been already initialized, need to re-init
+	assert.True(t, initDebug())
+
+	Log("foo")
+	Log("%s", testSecret("secret"))
+	Log("%s", testShort("toolong"))
+
+	buf, err := ioutil.ReadFile(fn)
+	require.NoError(t, err)
+
+	logStr := string(buf)
+	assert.Contains(t, logStr, "foo")
+	assert.NotContains(t, logStr, "secret")
+	assert.NotContains(t, logStr, "toolong")
+	assert.Contains(t, logStr, "shorter")
+}
+
+func TestDebugSecret(t *testing.T) {
+	td := t.TempDir()
+	t.Cleanup(func() {
+		initDebug()
+	})
+
+	fn := filepath.Join(td, "gopass.log")
+	t.Setenv("GOPASS_DEBUG_LOG", fn)
+	t.Setenv("GOPASS_DEBUG_LOG_SECRETS", "true")
+
+	// it's been already initialized, need to re-init
+	assert.True(t, initDebug())
+
+	assert.True(t, logSecrets)
+
+	Log("foo")
+	Log("%s", testSecret("secret"))
+
+	buf, err := ioutil.ReadFile(fn)
+	require.NoError(t, err)
+
+	logStr := string(buf)
+	assert.Contains(t, logStr, "foo")
+	assert.Contains(t, logStr, "secret")
+}
+
+func TestDebugFilter(t *testing.T) {
+	td := t.TempDir()
+	t.Cleanup(func() {
+		initDebug()
+	})
+
+	fn := filepath.Join(td, "gopass.log")
+	t.Setenv("GOPASS_DEBUG_LOG", fn)
+	t.Setenv("GOPASS_DEBUG_FUNCS", "TestDebugFilter")
+	t.Setenv("GOPASS_DEBUG_FILES", "debug_test.go")
+
+	buf := &bytes.Buffer{}
+	Stderr = buf
+	defer func() {
+		Stderr = os.Stderr
+	}()
+
+	// it's been already initialized, need to re-init
+	assert.True(t, initDebug())
+
+	Log("foo")
+	Log("%s", testSecret("secret"))
+
+	fbuf, err := ioutil.ReadFile(fn)
+	require.NoError(t, err)
+
+	logStr := string(fbuf)
+	assert.Contains(t, logStr, "foo")
+
+	stderrStr := buf.String()
+	assert.Contains(t, stderrStr, "TestDebugFilter")
 }

commit 1ca79864150cc08293a46fd26de28bfd9be70a2c

test: fix failing debug_test.go on Windows
--- FAIL: TestDebug (1.52s)
    testing.go:1097: TempDir RemoveAll cleanup: remove C:\Users\RUNNER~1\AppData\Local\Temp\TestDebug1334287763\001\gopass.log: The process cannot access the file because it is being used by another process.
--- FAIL: TestDebugSecret (1.90s)
    testing.go:1097: TempDir RemoveAll cleanup: remove C:\Users\RUNNER~1\AppData\Local\Temp\TestDebugSecret2374267425\001\gopass.log: The process cannot access the file because it is being used by another process.
--- FAIL: TestDebugFilter (1.77s)
    testing.go:1097: TempDir RemoveAll cleanup: remove C:\Users\RUNNER~1\AppData\Local\Temp\TestDebugFilter836798913\001\gopass.log: The process cannot access the file because it is being used by another process.

The gopass.log needs to be closed before it can be deleted on Windows.

Fixes: f584544 ("Increase test coverage (#2461)")
Signed-off-by: Eng Zer Jun <engzerjun@gmail.com>

pkg/debug/debug.go
@@ -20,9 +20,10 @@ var (
 )
 
 var opts struct {
-	logger *log.Logger
-	funcs  map[string]bool
-	files  map[string]bool
+	logger  *log.Logger
+	funcs   map[string]bool
+	files   map[string]bool
+	logFile *os.File
 }
 
 var logFn = doNotLog
@@ -62,6 +63,7 @@ func initDebugLogger() {
 		// seek to the end of the file (offset, whence [2 = end])
 		_, err := f.Seek(0, 2)
 		if err != nil {
+			_ = f.Close()
 			fmt.Fprintf(Stderr, "unable to seek to end of %v: %v\n", debugfile, err)
 			os.Exit(3)
 		}
@@ -76,6 +78,7 @@ func initDebugLogger() {
 		os.Exit(2)
 	}
 
+	opts.logFile = f
 	opts.logger = log.New(f, "", log.Ldate|log.Lmicroseconds)
 }
 
@@ -247,4 +250,13 @@ func doLog(offset int, f string, args ...any) {
 // IsEnabled returns true if debug logging was enabled.
 func IsEnabled() bool {
 	return enabled
+}
+
+// CloseLogfile closes the GOPASS_DEBUG_LOG file.
+func CloseLogfile() error {
+	if opts.logFile == nil {
+		return nil
+	}
+
+	return opts.logFile.Close()
 }

pkg/debug/debug_test.go
@@ -48,6 +48,7 @@ func TestDebug(t *testing.T) {
 	td := t.TempDir()
 	t.Cleanup(func() {
 		initDebug()
+		require.NoError(t, CloseLogfile())
 	})
 
 	fn := filepath.Join(td, "gopass.log")
@@ -74,6 +75,7 @@ func TestDebugSecret(t *testing.T) {
 	td := t.TempDir()
 	t.Cleanup(func() {
 		initDebug()
+		require.NoError(t, CloseLogfile())
 	})
 
 	fn := filepath.Join(td, "gopass.log")
@@ -100,6 +102,7 @@ func TestDebugFilter(t *testing.T) {
 	td := t.TempDir()
 	t.Cleanup(func() {
 		initDebug()
+		require.NoError(t, CloseLogfile())
 	})
 
 	fn := filepath.Join(td, "gopass.log")