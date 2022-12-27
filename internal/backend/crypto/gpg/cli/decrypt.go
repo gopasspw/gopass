da7a4b6876dfddc7f7e6188cd1b19094f699d914
Dominik Schulz <dominik.schulz@gauner.org>
2022-12-27 07:46:00 +0100

    Add timeout to gpg invocations (#2493)
    Fixes #1445
    
    Signed-off-by: Dominik Schulz <dominik.schulz@gauner.org>
    
    Signed-off-by: Dominik Schulz <dominik.schulz@gauner.org>

--- internal/backend/crypto/gpg/cli/decrypt.go
+++ internal/backend/crypto/gpg/cli/decrypt.go
@@ -11,6 +11,9 @@ import (
 
 // Decrypt will try to decrypt the given file.
 func (g *GPG) Decrypt(ctx context.Context, ciphertext []byte) ([]byte, error) {
+	ctx, cancel := context.WithTimeout(ctx, Timeout)
+	defer cancel()
+
 	args := append(g.args, "--decrypt")
 	cmd := exec.CommandContext(ctx, g.binary, args...)
 	cmd.Stdin = bytes.NewReader(ciphertext)
