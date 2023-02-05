4b7be8f212d8108ac870401d79b789da2862f93f
Dominik Schulz <dominik.schulz@gauner.org>
2023-02-05 12:24:03 +0100

    Fix tests and roll over updater key (#2532)
    Signed-off-by: Dominik Schulz <dominik.schulz@gauner.org>

--- internal/updater/verify_test.go
+++ internal/updater/verify_test.go
@@ -7,22 +7,23 @@ import (
 )
 
 // To generate use:
-// `gpg -u 0x2F752B2CA00248FC --armor --output testdata.sig --detach-sign testdata`.
+// `gpg -u 0x4B75BB5995892BE3 --armor --output testdata.sig --detach-sign testdata`.
 var testSignature = []byte(`
 -----BEGIN PGP SIGNATURE-----
 
-iQGzBAABCgAdFiEEeXE+gccft5Z7UYXQL3UrLKACSPwFAmAH4zYACgkQL3UrLKAC
-SPzcbgv9GwH+bxGJ2IwPPm2IsCCpZyef4Sa7PK8O/4lbEEDcpinap6LZvxzga+xb
-8pLV/lFj/QXkxQM3qJbdongkiBu5A77FUmgUi2j1pmK0g9ExF9mYKE858zBBcWIj
-P+NyVXQ4OIYmpmTvBa9h4Fbtb3R9J0gXdTh9v/8txAkKNEAWH8C+4lp+MHUmHC/N
-O/dwoYF+K7eFlFIei/E0+T2tT2TPgUX3DIxbBqucG9ZERBz6SfjJSjzpQ4R9jQvs
-VuUjbTpzmg1elY1aUN41VaiMHY4QGgDILwrs8Y63pRY65Yrox9wAYwxqxlpFbcHT
-5FZTJi2W1fKDecrewD7SJ4iEvXawa0DuWZp1bS5rQyk9OJTHudzYAbQuDece6S3E
-H5n6eSuBp+fooGijs56IbHQxTclHdRpLNCDRCMf/0vIAtnLy2mcMvIiUCzMolQtx
-umpKYk4AOrn1Q56ChcLVx7PIKu618UYq37Tnn7hZqjxL1mTDKcmhU1gPflbLRXiQ
-WubSpxPm
-=xTH+
+iQGzBAABCgAdFiEEwhyMrSlNNb9aO7sVs8WxoFYNhSIFAmPW2HQACgkQs8WxoFYN
+hSLmNgv8CLlft+O7vTolDPM/kZNOlM3UvAzbeA+JkeMyl7snWHnmWgtggtZhMIbq
+DIj1OjfW/JKiEqJZy2LCaXUxSshXJ2WHRfxTBvDprSQK5PHiVZwGmsJPXn1aOXSm
+OUCPNhv0/wl729reQ7VrLNVM6zXwY91+77XePMsKXV90Vdc+RXucEtZULNeZlhvE
+nnJ03ZHLeUpN61CJh6UhBP3dF1aFWiW4+oyONnNyxlC1QNh4oiwcP3iAe8m+gHWj
+cQ1z8sTyJFl2l6Mk9cq6wmMwhzyrPgxdre+YDa1oWb/hmq8U3qFH6kkaoYS6b9x3
+rEedoxQYh0N7B74IFSgjnKgtUkfQPRXFEUfbGpz03NVtwMnqg8IiCO5bMcbHiqDG
+UezkZM1wpxVWCgoGcZaCv6c1gu4KAx8iVhovxekJcEVf+BUahMWBPwIjsmJg5kCU
+L63+5ieE6wuYAcVPKZBUG3v9J6VjKpc3puv0sKUPw6swYOF973u3vfChs0oHjpiS
+/+EULiNZ
+=dS8F
 -----END PGP SIGNATURE-----
+
 `)
 
 var testData = []byte(`gopass-sign-test
