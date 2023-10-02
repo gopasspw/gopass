788f57723e25703b31d021c2907c5781934ad86f
Dominik Schulz <dominik.schulz@gauner.org>
2023-10-02 14:54:30 +0200

    [enhancement] Add input validation to block illegal mount points (#2672)
    This commit adds a check to prevent mounting mount points
    that can not be used later on.
    
    Signed-off-by: Dominik Schulz <dominik.schulz@gauner.org>

--- internal/store/root/mount_test.go
+++ internal/store/root/mount_test.go
@@ -2,6 +2,7 @@ package root
 
 import (
 	"context"
+	"runtime"
 	"testing"
 
 	"github.com/gopasspw/gopass/pkg/ctxutil"
@@ -51,4 +52,24 @@ func TestMountPoint(t *testing.T) {
 	assert.NoError(t, rs.AddMount(ctx, "sub2", u.StoreDir("sub2")))
 
 	assert.Equal(t, "sub1", rs.MountPoint("sub1"))
+}
+
+func TestMountPointIllegal(t *testing.T) {
+	u := gptest.NewUnitTester(t)
+
+	ctx := context.Background()
+	ctx = ctxutil.WithAlwaysYes(ctx, true)
+	ctx = ctxutil.WithHidden(ctx, true)
+
+	rs, err := createRootStore(ctx, u)
+	require.NoError(t, err)
+
+	assert.NoError(t, u.InitStore("sub1"))
+	assert.NoError(t, u.InitStore("sub2"))
+	assert.NoError(t, rs.AddMount(ctx, "sub1/foo", u.StoreDir("sub1")))
+	if runtime.GOOS == "windows" {
+		assert.NoError(t, rs.AddMount(ctx, "sub2\\foo", u.StoreDir("sub2")))
+		assert.Error(t, rs.AddMount(ctx, "sub2\\", u.StoreDir("sub2")))
+	}
+	assert.Error(t, rs.AddMount(ctx, "sub2/", u.StoreDir("sub2")))
 }
