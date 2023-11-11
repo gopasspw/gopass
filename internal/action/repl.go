39ecb3837205c8efacbc331a935b93490bcc4460
Dominik Schulz <dominik.schulz@gauner.org>
2023-11-11 10:40:17 +0100

    Replace chzyer/readline with ergochat/readline (#2684)
    * Replace chzyer/readline with ergochat/readline
    
    The former seemed unmaintained and was lacking patches required for
    OpenBSD.
    
    See GH-2683
    
    Signed-off-by: Dominik Schulz <dominik.schulz@gauner.org>
    
    * Add an error message when using clear on windows.
    
    Signed-off-by: Dominik Schulz <dominik.schulz@gauner.org>
    
    * use readline 0.0.6's ClearScreen method
    
    ---------
    
    Signed-off-by: Dominik Schulz <dominik.schulz@gauner.org>
    Co-authored-by: Shivaram Lingamneni <slingamn@cs.stanford.edu>

--- internal/action/repl.go
+++ internal/action/repl.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/chzyer/readline"
+	"github.com/ergochat/readline"
 	"github.com/gopasspw/gopass/internal/out"
 	"github.com/gopasspw/gopass/internal/tree"
 	"github.com/gopasspw/gopass/pkg/debug"
@@ -139,7 +139,18 @@ READ:
 			return fmt.Errorf("user aborted")
 		default:
 		}
-		rl.Config.AutoComplete = s.prefixCompleter(c)
+
+		// we need to update the completer on every loop since
+		// the list of secrets may have changed, e.g. due to
+		// the user adding a new secret.
+		cfg := rl.GetConfig()
+		cfg.AutoComplete = s.prefixCompleter(c)
+		if err := rl.SetConfig(cfg); err != nil {
+			debug.Log("Failed to set readline config: %s", err)
+
+			break
+		}
+
 		line, err := rl.Readline()
 		if err != nil {
 			debug.Log("Readline error: %s", err)
@@ -163,7 +174,7 @@ READ:
 
 			continue
 		case "clear":
-			readline.ClearScreen(stdout) //nolint:errcheck
+			rl.ClearScreen()
 
 			continue
 		default:
