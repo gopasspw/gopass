ef434d97028767211a4fbd3146b1523a99044d32
Dominik Schulz <dominik.schulz@gmail.com>
2025-02-28 09:10:31 +0000

    Fix lint error

--- internal/tpl/funcs_test.go
+++ internal/tpl/funcs_test.go
@@ -11,73 +11,73 @@ import (
 
 func TestMd5sum(t *testing.T) {
 	result, err := md5sum()("test")
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	assert.Equal(t, hashsum.MD5Hex("test"), result)
 }
 
 func TestSha1sum(t *testing.T) {
 	result, err := sha1sum()("test")
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	assert.Equal(t, hashsum.SHA1Hex("test"), result)
 }
 
 func TestSha256sum(t *testing.T) {
 	result, err := sha256sum()("test")
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	assert.Equal(t, hashsum.SHA256Hex("test"), result)
 }
 
 func TestSha512sum(t *testing.T) {
 	result, err := sha512sum()("test")
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	assert.Equal(t, hashsum.SHA512Hex("test"), result)
 }
 
 func TestBlake3sum(t *testing.T) {
 	result, err := blake3sum()("test")
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	assert.Equal(t, hashsum.Blake3Hex("test"), result)
 }
 
 func TestMd5cryptFunc(t *testing.T) {
 	result, err := md5cryptFunc()("salt", "password")
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	assert.True(t, strings.HasPrefix(result, "{MD5-CRYPT}"), result)
 }
 
 func TestSshaFunc(t *testing.T) {
 	result, err := sshaFunc()("salt", "password")
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	assert.True(t, strings.HasPrefix(result, "{SSHA}"), result)
 }
 
 func TestSsha256Func(t *testing.T) {
 	result, err := ssha256Func()("salt", "password")
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	assert.True(t, strings.HasPrefix(result, "{SSHA256}"), result)
 }
 
 func TestSsha512Func(t *testing.T) {
 	result, err := ssha512Func()("salt", "password")
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	assert.True(t, strings.HasPrefix(result, "{SSHA512}"), result)
 }
 
 func TestArgon2iFunc(t *testing.T) {
 	result, err := argon2iFunc()("salt", "password")
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	assert.True(t, strings.HasPrefix(result, "{ARGON2I}$argon2i$"), result)
 }
 
 func TestArgon2idFunc(t *testing.T) {
 	result, err := argon2idFunc()("salt", "password")
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	assert.True(t, strings.HasPrefix(result, "{ARGON2ID}$argon2id$"), result)
 }
 
 func TestBcryptFunc(t *testing.T) {
 	result, err := bcryptFunc()("password")
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	assert.True(t, strings.HasPrefix(result, "{BLF-CRYPT}$2a$"))
 }
 
