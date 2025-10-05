b10b940aace0972e7f20eb2c026bb66159674c17
sarff <30344665+sarff@users.noreply.github.com>
2025-10-05 21:16:33 +0300

    x3 optimization for hash (#3269)
    * [fix] Fix version check (#3268)
    
    Previously `gopass version` would always print an upgrade notice
    when build from source even if there were no newer releases.
    
    Signed-off-by: Dominik Schulz <dominik.schulz@gauner.org>
    Signed-off-by: dmit <exehoua@gmail.com>
    
    * x3 optimization for hash
    
    Signed-off-by: dmit <exehoua@gmail.com>
    
    * signed-off
    
    Signed-off-by: dmit <exehoua@gmail.com>
    
    ---------
    
    Signed-off-by: Dominik Schulz <dominik.schulz@gauner.org>
    Signed-off-by: dmit <exehoua@gmail.com>
    Co-authored-by: Dominik Schulz <dominik.schulz@gauner.org>

--- internal/hashsum/hashsums.go
+++ internal/hashsum/hashsums.go
@@ -6,27 +6,44 @@ import (
 	"crypto/sha1"
 	"crypto/sha256"
 	"crypto/sha512"
-	"fmt"
+	"encoding/hex"
 
 	"github.com/zeebo/blake3"
 )
 
+// MD5Hex returns the MD5 hash of the input string.
+// Note: MD5 is cryptographically broken and should not be used for security purposes.
 func MD5Hex(in string) string {
-	return fmt.Sprintf("%x", md5.Sum([]byte(in)))
+	hash := md5.Sum([]byte(in))
+
+	return hex.EncodeToString(hash[:])
 }
 
+// SHA1Hex returns the SHA-1 hash of the input string.
+// Note: SHA-1 is cryptographically broken and should not be used for security purposes.
 func SHA1Hex(in string) string {
-	return fmt.Sprintf("%x", sha1.Sum([]byte(in)))
+	hash := sha1.Sum([]byte(in))
+
+	return hex.EncodeToString(hash[:])
 }
 
+// SHA256Hex returns the SHA-256 hash of the input string.
 func SHA256Hex(in string) string {
-	return fmt.Sprintf("%x", sha256.Sum256([]byte(in)))
+	hash := sha256.Sum256([]byte(in))
+
+	return hex.EncodeToString(hash[:])
 }
 
+// SHA512Hex returns the SHA-512 hash of the input string.
 func SHA512Hex(in string) string {
-	return fmt.Sprintf("%x", sha512.Sum512([]byte(in)))
+	hash := sha512.Sum512([]byte(in))
+
+	return hex.EncodeToString(hash[:])
 }
 
+// Blake3Hex returns the BLAKE3 hash of the input string.
 func Blake3Hex(in string) string {
-	return fmt.Sprintf("%x", blake3.Sum256([]byte(in)))
+	hash := blake3.Sum256([]byte(in))
+
+	return hex.EncodeToString(hash[:])
 }
