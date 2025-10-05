0e25d91a29df6ad09083971bc489a82b28cbd08d
dmit <exehoua@gmail.com>
2025-10-05 16:51:16 +0300

    x3 optimization for hash
    Signed-off-by: dmit <exehoua@gmail.com>

--- internal/hashsum/hashsums.go
+++ internal/hashsum/hashsums.go
@@ -6,27 +6,34 @@ import (
 	"crypto/sha1"
 	"crypto/sha256"
 	"crypto/sha512"
-	"fmt"
+	"encoding/hex"
 
 	"github.com/zeebo/blake3"
 )
 
+// Note: MD5 is cryptographically broken and should not be used for security purposes.
 func MD5Hex(in string) string {
-	return fmt.Sprintf("%x", md5.Sum([]byte(in)))
+	hash := md5.Sum([]byte(in))
+	return hex.EncodeToString(hash[:])
 }
 
+// Note: SHA-1 is cryptographically broken and should not be used for security purposes.
 func SHA1Hex(in string) string {
-	return fmt.Sprintf("%x", sha1.Sum([]byte(in)))
+	hash := sha1.Sum([]byte(in))
+	return hex.EncodeToString(hash[:])
 }
 
 func SHA256Hex(in string) string {
-	return fmt.Sprintf("%x", sha256.Sum256([]byte(in)))
+	hash := sha256.Sum256([]byte(in))
+	return hex.EncodeToString(hash[:])
 }
 
 func SHA512Hex(in string) string {
-	return fmt.Sprintf("%x", sha512.Sum512([]byte(in)))
+	hash := sha512.Sum512([]byte(in))
+	return hex.EncodeToString(hash[:])
 }
 
 func Blake3Hex(in string) string {
-	return fmt.Sprintf("%x", blake3.Sum256([]byte(in)))
+	hash := blake3.Sum256([]byte(in))
+	return hex.EncodeToString(hash[:])
 }
