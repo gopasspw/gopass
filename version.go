996c966f94d386fd69936b1b87b99257f811e525
Dominik Schulz <dominik.schulz@gauner.org>
2026-09-10 08:35:20 +0200

    Tag v1.17.2-rc.0
    Signed-off-by: Dominik Schulz <dominik.schulz@gauner.org>

--- version.go
+++ version.go
@@ -15,10 +15,10 @@ func getVersion() semver.Version {
 	return semver.Version{
 		Major: 1,
 		Minor: 17,
-		Patch: 1,
+		Patch: 2,
 		Pre: []semver.PRVersion{
 			{VersionStr: "git"},
 		},
-		Build: []string{"b9769532"},
+		Build: []string{"34674c90"},
 	}
 }
